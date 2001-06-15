d872a28952c086e8f0521a3b93c3f973164facb4
adcockj <adcockj@users.sourceforge.net>
2001-06-15 16:25:05 +0000

    Updated memcpy functions
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@677 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/FD_CommonFunctions.asm
+++ DScaler/FD_CommonFunctions.asm
@@ -528,15 +528,13 @@ memcpyBOBSSE_End:
 
 public _memcpyMMX
 
-	Dest	equ	[esp+4+16]
-	Src	    equ	[esp+8+16]
-	nBytes	equ	[esp+12+16]
+	Dest	equ	[esp+12]
+	Src	    equ	[esp+16]
+	nBytes	equ	[esp+20]
 
 _memcpyMMX:
-	push	ebp
 	push	edi
 	push	esi
-	push	ebx
 
 	mov		esi, dword ptr[Src]
 	mov		edi, dword ptr[Dest]
@@ -569,50 +567,144 @@ Memcpy_Loop:
 	shr     ecx, 2
 	cmp     ecx, 0
 	je Memcpy_End
-align 8
-Memcpy_Loop2:
-	mov edx, [esi] 
-	mov [edi], edx
-	add esi, 4
-	add edi, 4
-	loop Memcpy_Loop2
+	rep movsd
 
 Memcpy_End:
-	pop	ebx
 	pop	esi
 	pop	edi
-	pop	ebp
 	ret	
 
 
 ;////////////////////////////////////////////////////////////////////
 ; void memcpySSE(void *Dest, void *Src, size_t nBytes);
 ;
-; On SSE machines, we can use the 128-bit floating-point registers and
+; On SSE machines we use the 
 ; bypass write caching to copy a bit faster.  The destination has to be
 ; 16-byte aligned.  
 ;////////////////////////////////////////////////////////////////////
 public _memcpySSE
 
-	Dest	equ	[esp+4+16]
-	Src	    equ	[esp+8+16]
-	nBytes	equ	[esp+12+16]
+	Dest	equ	[esp+12]
+	Src	    equ	[esp+16]
+	nBytes	equ	[esp+20]
 
 _memcpySSE:
-	push	ebp
 	push	edi
 	push	esi
-	push	ebx
 
-	mov		edi, dword ptr[Dest]
 	mov		esi, dword ptr[Src]
+	mov		edi, dword ptr[Dest]
 	mov		ecx, nBytes
-	rep movsb 
-	pop	ebx
+	shr     ecx, 7
+align 4
+MemcpySSE_Loop:
+	; movaps should be slightly more efficient
+	; as the data is 16 bit aligned
+	movaps	xmm0, [esi]
+	movaps	xmm1, [esi+16*1]
+	movaps	xmm2, [esi+16*2]
+	movaps	xmm3, [esi+16*3]
+	movaps	xmm4, [esi+16*4]
+	movaps	xmm5, [esi+16*5]
+	movaps	xmm6, [esi+16*6]
+	movaps	xmm7, [esi+16*7]
+	movntps	[edi], xmm0
+	movntps	[edi+16*1], xmm1
+	movntps	[edi+16*2], xmm2
+	movntps	[edi+16*3], xmm3
+	movntps	[edi+16*4], xmm4
+	movntps	[edi+16*5], xmm5
+	movntps	[edi+16*6], xmm6
+	movntps	[edi+16*7], xmm7
+	add		esi, 128
+	add		edi, 128
+	dec ecx
+	jnz MemcpySSE_Loop
+
+	mov		ecx, nBytes
+	and     ecx, 127
+	shr     ecx, 2
+	cmp     ecx, 0
+	je MemcpySSE_End
+	rep movsd
+
+MemcpySSE_End:
 	pop	esi
 	pop	edi
-	pop	ebp
 	ret	
 
 	end
 
+;////////////////////////////////////////////////////////////////////
+; void memcpyAMD(void *Dest, void *Src, size_t nBytes);
+;
+; Comes from AMD Site
+;////////////////////////////////////////////////////////////////////
+
+public _memcpyAMD
+
+	Dest	equ	[esp+12]
+	Src	    equ	[esp+16]
+	nBytes	equ	[esp+20]
+
+_memcpyAMD:
+	push	edi
+	push	esi
+
+	mov		esi, dword ptr[Src]
+	mov		edi, dword ptr[Dest]
+	mov		ecx, nBytes
+	shr     ecx, 6                      ; nBytes / 64
+align 8
+Memcpy_Loop:
+	movq mm0,[esi+0]
+	movq mm1,[esi+8]
+	movq [edi+0],mm0
+	movq [edi+8],mm1
+	movq mm2,[esi+16]
+	movq mm3,[esi+24]
+	movq [edi+16],mm2
+	movq [edi+24],mm3
+	movq mm0,[esi+32]
+	movq mm1,[esi+40]
+	movq [edi+32],mm0
+	movq [edi+40],mm1
+	movq mm2,[esi+48]
+	movq mm3,[esi+56]
+	movq [edi+48],mm2
+	movq [edi+56],mm3
+	add		esi, 64
+	add		edi, 64
+	loop Memcpy_Loop
+
+	mov ecx, nBytes
+	shr ecx,2               ; dword count
+	and ecx,1111b
+	neg ecx
+	add ecx, Memcpy_End
+	jmp ecx
+
+align 4
+	movsd
+	movsd                   ; perform last 1-15 dword copies
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd                   ; perform last 1-7 dword copies
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd
+	movsd
+
+Memcpy_End:
+	pop	esi
+	pop	edi
+	ret	
+	
+	end

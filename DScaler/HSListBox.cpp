5c1a25c323ae1a58b2ce9e7688a653a7e18e98d2
tobbej <tobbej@users.sourceforge.net>
2001-06-24 14:05:46 +0000

    A CListBox class with a horizontal scrollbar
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@699 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ DScaler/HSListBox.cpp
@@ -0,0 +1,114 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: HSListBox.cpp,v 1.1 2001-06-24 14:05:46 tobbej Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2000 John Adcock.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//	This file is subject to the terms of the GNU General Public License as
+//	published by the Free Software Foundation.  A copy of this license is
+//	included with this software distribution in the file COPYING.  If you
+//	do not have a copy, you may obtain a copy by writing to the Free
+//	Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//	This software is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 24 Jun 2001   Torbjörn Jansson      Initial release
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "HSListBox.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CHSListBox
+
+CHSListBox::CHSListBox()
+:m_nMaxWidth(0)
+{
+}
+
+CHSListBox::~CHSListBox()
+{
+}
+
+int CHSListBox::AddString(LPCTSTR lpszItem)
+{
+	int nRet = CListBox::AddString(lpszItem);
+
+	/*
+	SCROLLINFO scrollInfo;
+	memset(&scrollInfo, 0, sizeof(SCROLLINFO));
+	scrollInfo.cbSize = sizeof(SCROLLINFO);
+	scrollInfo.fMask  = SIF_ALL;
+	GetScrollInfo(SB_VERT, &scrollInfo, SIF_ALL);
+
+	//is the vertical scrollbar visibel?
+	int nScrollWidth = 0;
+	if(GetCount() > 1 && ((int)scrollInfo.nMax >= (int)scrollInfo.nPage))
+	{
+		nScrollWidth = GetSystemMetrics(SM_CXVSCROLL);
+	}*/
+	if(nRet!=LB_ERR && nRet!=LB_ERRSPACE)
+	{
+		m_nMaxWidth = max(m_nMaxWidth, GetTextSize(lpszItem).cx);
+		SetHorizontalExtent(m_nMaxWidth + 4);
+	}
+
+	return nRet;
+}
+int CHSListBox::InsertString(int nIndex, LPCTSTR lpszItem)
+{
+	int nRet=CListBox::InsertString(nIndex,lpszItem);
+	
+	if(nRet!=LB_ERR && nRet!=LB_ERRSPACE)
+	{
+		m_nMaxWidth = max(m_nMaxWidth, GetTextSize(lpszItem).cx);
+		SetHorizontalExtent(m_nMaxWidth + 4);
+	}
+
+	return nRet;
+}
+void CHSListBox::ResetContent()
+{
+	CListBox::ResetContent();
+	m_nMaxWidth=0;
+	SetHorizontalExtent(0);
+}
+
+
+BEGIN_MESSAGE_MAP(CHSListBox, CListBox)
+	//{{AFX_MSG_MAP(CHSListBox)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+/////////////////////////////////////////////////////////////////////////////
+// CHSListBox message handlers
+
+
+CSize CHSListBox::GetTextSize(LPCTSTR lpszItem)
+{
+	CSize sSize;
+	CClientDC myDC(this);
+
+	CFont* pListBoxFont = GetFont();
+    if(pListBoxFont != NULL)
+    {
+		CFont* pOldFont = myDC.SelectObject(pListBoxFont);
+		GetTextExtentPoint32(myDC.m_hDC, lpszItem, strlen(lpszItem), &sSize);
+		myDC.SelectObject(pOldFont);
+	}
+	return sSize;
+}
---
+++ DScaler/HSListBox.h
@@ -0,0 +1,82 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: HSListBox.h,v 1.1 2001-06-24 14:05:46 tobbej Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2000 John Adcock.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//	This file is subject to the terms of the GNU General Public License as
+//	published by the Free Software Foundation.  A copy of this license is
+//	included with this software distribution in the file COPYING.  If you
+//	do not have a copy, you may obtain a copy by writing to the Free
+//	Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//	This software is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//	GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 24 Jun 2001   Torbjörn Jansson      Initial release
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(AFX_HSLISTBOX_H__3CCC6FDD_B7F9_486D_A66D_98E0D8B92C0C__INCLUDED_)
+#define AFX_HSLISTBOX_H__3CCC6FDD_B7F9_486D_A66D_98E0D8B92C0C__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+/*
+ A CListBox derived class with a horizontal scrollbar.
+ 
+ If you call any memberfunction that modifies the strings that is not implemented in this class
+ the horizontal scrollbar might become out of sync with the text in it.
+
+ It shodnt be any problem implementing the other functions if we need it.
+*/
+
+class CHSListBox : public CListBox
+{
+// Construction
+public:
+	CHSListBox();
+
+// Attributes
+public:
+
+// Operations
+public:
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CHSListBox)
+	//}}AFX_VIRTUAL
+
+// Implementation
+public:
+	void ResetContent( );
+	virtual ~CHSListBox();
+	int AddString(LPCTSTR lpszItem);
+	int InsertString(int nIndex, LPCTSTR lpszItem);
+
+// Generated message map functions
+protected:
+	int m_nMaxWidth;
+	//{{AFX_MSG(CHSListBox)
+	//}}AFX_MSG
+
+	DECLARE_MESSAGE_MAP()
+private:
+	CSize GetTextSize(LPCTSTR lpszItem);
+};
+
+/////////////////////////////////////////////////////////////////////////////
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_HSLISTBOX_H__3CCC6FDD_B7F9_486D_A66D_98E0D8B92C0C__INCLUDED_)

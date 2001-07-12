141b38223b10cac05685c3bd5882781f1eccdf85
adcockj <adcockj@users.sourceforge.net>
2001-07-12 16:26:59 +0000

    Added CVS id and log
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@748 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/StdAfx.cpp
+++ DScaler/StdAfx.cpp
@@ -1,8 +1,23 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	PlugTest.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
+/////////////////////////////////////////////////////////////////////////////
+// $Id: StdAfx.cpp,v 1.2 2001-07-12 16:26:59 adcockj Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2000 John Adcock.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// reference any additional headers you need in STDAFX.H
+// and not in this file
+//////////////////////////////////////////////////////////////////////////////
 
 #include "stdafx.h"
 
-// TODO: reference any additional headers you need in STDAFX.H
-// and not in this file

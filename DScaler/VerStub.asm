2a30e592bc9e092f40a992252ec4f475de0b8b85
adcockj <adcockj@users.sourceforge.net>
2001-07-27 17:43:00 +0000

    Up build to 2
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@795 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/VerStub.asm
+++ DScaler/VerStub.asm
@@ -5,7 +5,7 @@
 	public	_gBuildNum
 	public	_gBuildDateTime
 
-_gBuildNum	dd	1
-_gBuildDateTime	db	"Fri Jul 20 13:48:54 2001",0
+_gBuildNum	dd	2
+_gBuildDateTime	db	"Fri Jul 27 18:38:36 2001",0
 
 	end

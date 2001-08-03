44a80de0518e4350cbd361700c457c1472d4152a
adcockj <adcockj@users.sourceforge.net>
2001-08-03 12:27:17 +0000

    Added Sharpness filter (copy of code from GreedyH)
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@848 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ Plugins/FLT_Sharpness/FLT_Sharpness.c
@@ -0,0 +1,165 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: FLT_Sharpness.c,v 1.1 2001-08-03 12:27:17 adcockj Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2001 Tom Barry.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+#include "windows.h"
+#include "DS_Filter.h"
+
+long Sharpness = 10;
+
+long FilterSharpness(DEINTERLACE_INFO *info)
+{
+    short *Pixels;
+    int y;
+    __int64 qwYMask = 0x00ff00ff00ff00ff;
+    __int64 qwSharpness;
+    int Cycles;
+
+    // Need to have the current field to do the filtering.
+    // and if we have nothing to do just return
+    if ((info->IsOdd && info->OddLines[0] == NULL) ||
+        (! info->IsOdd && info->EvenLines[0] == NULL) || 
+        Sharpness == 0)
+    {
+        return 1000;
+    }
+
+    qwSharpness = Sharpness;
+    qwSharpness |= qwSharpness << 48 | qwSharpness << 32 | qwSharpness << 16;
+    Cycles = info->LineLength / 8 - 2;
+
+    for (y = 0; y < info->FieldHeight; y++)
+    {
+        if (info->IsOdd)
+        {
+            Pixels = info->OddLines[0][y];
+        }
+        else
+        {
+            Pixels = info->EvenLines[0][y];
+        }
+
+        _asm
+        {
+            mov eax, Pixels
+            movq mm1, [eax]
+            add eax, 8
+            movq mm2, [eax]
+            mov ecx, Cycles
+            movq mm5, qwYMask
+            movq mm4, qwSharpness
+LOOP_LABEL:
+            movq mm0, mm1
+            movq mm1, mm2
+            movq mm2, [eax + 8]
+
+            // do edge enhancement. 
+		    movq	mm7, mm1				// work copy of curr pixel val
+		    psrlq   mm0, 48					// right justify 1 pixel from qword to left
+		    psllq   mm7, 16                 // left justify 3 pixels
+		    por     mm0, mm7				// and combine
+		    
+		    movq	mm6, mm2				// copy of right qword pixel val
+		    movq	mm7, mm1                // another copy of L2N current
+		    psllq	mm6, 48					// left just 1 pixel from qword to right
+		    psrlq   mm7, 16					// right just 3 pixels
+		    por		mm6, mm7				// combine
+		    pavgb	mm0, mm6				// avg of forward and prev by 1 pixel
+
+            // we handle the possible plus and minus sharpness adjustments separately
+		    movq    mm7, mm1				// another copy of L2N
+		    psubusb mm7, mm0				// curr - surround
+		    psubusb mm0, mm1                // surround - curr
+		    pand	mm7, mm5                // YMask
+		    pand	mm0, mm5                // YMask
+		    pmullw  mm7, mm4                // mult by sharpness factor
+		    pmullw  mm0, mm4                // mult by sharpness factor
+		    psrlw   mm7, 8					// now have diff*EdgeEnhAmt/256 ratio			
+		    psrlw   mm0, 8					// now have diff*EdgeEnhAmt/256 ratio			
+
+		    paddusb mm7, mm1				// edge enhancement up
+		    psubusb mm7, mm0                // edge enhancement down, mm7 now our sharpened value
+
+            movq [eax], mm7
+            add eax, 8
+            dec ecx
+            jne near LOOP_LABEL
+        }
+    }
+    _asm 
+    {
+        emms
+    }
+    return 1000;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Start of Settings related code
+/////////////////////////////////////////////////////////////////////////////
+SETTING FLT_SharpnessSettings[FLT_SHARPNESS_SETTING_LASTONE] =
+{
+    {
+        "Sharpness", SLIDER, 0, &Sharpness,
+        128, 0, 255, 10, 1000,
+        NULL,
+        "SharpnessFilter", "Sharpness", NULL,
+    },
+};
+
+FILTER_METHOD SharpnessMethod =
+{
+    sizeof(FILTER_METHOD),
+    FILTER_CURRENT_VERSION,
+    "Sharpness Filter",
+    "&Sharpness",
+    FALSE,
+    TRUE,
+    FilterSharpness, 
+    0,
+    TRUE,
+    NULL,
+    NULL,
+    NULL,
+    FLT_SHARPNESS_SETTING_LASTONE,
+    FLT_SharpnessSettings,
+    WM_FLT_SHARPNESS_GETVALUE - WM_USER,
+};
+
+
+__declspec(dllexport) FILTER_METHOD* GetFilterPluginInfo(long CpuFeatureFlags)
+{
+    // this plugin need as decent processor
+    // as were going to need an acurate average
+    if(CpuFeatureFlags & FEATURE_MMXEXT || CpuFeatureFlags & FEATURE_SSE)
+    {
+        return &SharpnessMethod;
+    }
+    else
+    {
+        return NULL;
+    }
+}
+
+BOOL WINAPI _DllMainCRTStartup(HANDLE hInst, ULONG ul_reason_for_call, LPVOID lpReserved)
+{
+    return TRUE;
+}
+
---
+++ Plugins/FLT_Sharpness/FLT_Sharpness.dsp
@@ -0,0 +1,109 @@
+# Microsoft Developer Studio Project File - Name="FLT_Sharpness" - Package Owner=<4>
+# Microsoft Developer Studio Generated Build File, Format Version 6.00
+# ** DO NOT EDIT **
+
+# TARGTYPE "Win32 (x86) Dynamic-Link Library" 0x0102
+
+CFG=FLT_Sharpness - Win32 Debug
+!MESSAGE This is not a valid makefile. To build this project using NMAKE,
+!MESSAGE use the Export Makefile command and run
+!MESSAGE 
+!MESSAGE NMAKE /f "FLT_Sharpness.mak".
+!MESSAGE 
+!MESSAGE You can specify a configuration when running NMAKE
+!MESSAGE by defining the macro CFG on the command line. For example:
+!MESSAGE 
+!MESSAGE NMAKE /f "FLT_Sharpness.mak" CFG="FLT_Sharpness - Win32 Debug"
+!MESSAGE 
+!MESSAGE Possible choices for configuration are:
+!MESSAGE 
+!MESSAGE "FLT_Sharpness - Win32 Release" (based on "Win32 (x86) Dynamic-Link Library")
+!MESSAGE "FLT_Sharpness - Win32 Debug" (based on "Win32 (x86) Dynamic-Link Library")
+!MESSAGE 
+
+# Begin Project
+# PROP AllowPerConfigDependencies 0
+# PROP Scc_ProjName ""
+# PROP Scc_LocalPath ""
+CPP=cl.exe
+MTL=midl.exe
+RSC=rc.exe
+
+!IF  "$(CFG)" == "FLT_Sharpness - Win32 Release"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 0
+# PROP BASE Output_Dir "Release"
+# PROP BASE Intermediate_Dir "Release"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 0
+# PROP Output_Dir "Release"
+# PROP Intermediate_Dir "Release"
+# PROP Ignore_Export_Lib 1
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /MT /W3 /GX /O2 /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "FLT_Sharpness_EXPORTS" /YX /FD /c
+# ADD CPP /nologo /MD /W3 /Ox /Ot /Ow /Og /Op /Ob2 /I "..\..\Api" /D "WIN32" /D "NDEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "FLT_Sharpness_EXPORTS" /FD /c
+# SUBTRACT CPP /Os /YX
+# ADD BASE MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "NDEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x809 /d "NDEBUG"
+# ADD RSC /l 0x809 /d "NDEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /machine:I386
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /pdb:none /machine:I386 /out:"..\..\Release/FLT_Sharpness.dll" /OPT:NOWIN98
+
+!ELSEIF  "$(CFG)" == "FLT_Sharpness - Win32 Debug"
+
+# PROP BASE Use_MFC 0
+# PROP BASE Use_Debug_Libraries 1
+# PROP BASE Output_Dir "Debug"
+# PROP BASE Intermediate_Dir "Debug"
+# PROP BASE Target_Dir ""
+# PROP Use_MFC 0
+# PROP Use_Debug_Libraries 1
+# PROP Output_Dir "Debug"
+# PROP Intermediate_Dir "Debug"
+# PROP Ignore_Export_Lib 0
+# PROP Target_Dir ""
+# ADD BASE CPP /nologo /MTd /W3 /Gm /GX /ZI /Od /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "FLT_Sharpness_EXPORTS" /YX /FD /GZ /c
+# ADD CPP /nologo /MDd /W3 /Gm /Zi /Ox /Ot /Ow /Og /Op /I "..\..\Api" /D "WIN32" /D "_DEBUG" /D "_WINDOWS" /D "_MBCS" /D "_USRDLL" /D "FLT_Sharpness_EXPORTS" /FD /c
+# SUBTRACT CPP /Oi
+# ADD BASE MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD MTL /nologo /D "_DEBUG" /mktyplib203 /win32
+# ADD BASE RSC /l 0x809 /d "_DEBUG"
+# ADD RSC /l 0x409 /d "_DEBUG"
+BSC32=bscmake.exe
+# ADD BASE BSC32 /nologo
+# ADD BSC32 /nologo
+LINK32=link.exe
+# ADD BASE LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /debug /machine:I386 /pdbtype:sept
+# ADD LINK32 kernel32.lib user32.lib gdi32.lib winspool.lib comdlg32.lib advapi32.lib shell32.lib ole32.lib oleaut32.lib uuid.lib odbc32.lib odbccp32.lib /nologo /dll /incremental:no /debug /machine:I386 /out:"..\..\Debug/FLT_Sharpness.dll" /pdbtype:sept
+
+!ENDIF 
+
+# Begin Target
+
+# Name "FLT_Sharpness - Win32 Release"
+# Name "FLT_Sharpness - Win32 Debug"
+# Begin Group "Source Files"
+
+# PROP Default_Filter "cpp;c;cxx;rc;def;r;odl;idl;hpj;bat"
+# Begin Source File
+
+SOURCE=.\FLT_Sharpness.c
+# End Source File
+# End Group
+# Begin Group "Header Files"
+
+# PROP Default_Filter "h;hpp;hxx;hm;inl"
+# End Group
+# Begin Group "Resource Files"
+
+# PROP Default_Filter "ico;cur;bmp;dlg;rc2;rct;bin;rgs;gif;jpg;jpeg;jpe"
+# End Group
+# End Target
+# End Project

56e68e01280a4e1a536e25593f1857483d6140a4
adcockj <adcockj@users.sourceforge.net>
2001-08-09 07:57:15 +0000

    Increment build
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@894 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/VerStub.asm
+++ DScaler/VerStub.asm
@@ -5,7 +5,7 @@
 	public	_gBuildNum
 	public	_gBuildDateTime
 
-_gBuildNum	dd	3
-_gBuildDateTime	db	"Sun Jul 29 11:03:40 2001",0
+_gBuildNum	dd	4
+_gBuildDateTime	db	"Thu Aug 09 08:59:16 2001",0
 
 	end

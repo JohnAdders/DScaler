a2863d051f52328c6564c70f7aa1ea9907faf84c
adcockj <adcockj@users.sourceforge.net>
2001-09-11 12:02:28 +0000

    Updated Help files Added web support files so that html files are shared
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1054 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ Help/index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>Dscaler Help</title>
+</head>
+<frameset rows="*,60">
+    <frameset cols="200,*">
+         <frame name="contents" target="main" src="HelpContents.htm" scrolling="no">
+         <frame name="main" src="DScaler_Readme.htm" scrolling="auto">
+    </frameset>
+    <frame name="Nav" target="_top" src="Nav.htm" scrolling="no">
+<noframes>
+<body>
+<p>This page uses frames, but your browser doesn't support
+them.</p>
+</body>
+</noframes>
+</frameset>
+</html>
+

183eac51198f042e8c3d8886c0233d9ee663a70c
adcockj <adcockj@users.sourceforge.net>
2001-10-23 08:53:32 +0000

    Updated version
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1134 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/VerStub.asm
+++ DScaler/VerStub.asm
@@ -5,7 +5,7 @@
 	public	_gBuildNum
 	public	_gBuildDateTime
 
-_gBuildNum	dd	10
-_gBuildDateTime	db	"Sat Oct 06 18:01:33 2001",0
+_gBuildNum	dd	2574
+_gBuildDateTime	db	"Tue Oct 23 09:49:39 2001",0
 
 	end

5671133a3075730f5bcb8da1f0b45da00405423a
Laurent Garnier <laurentg@users.sourceforge.net>
2001-11-24 00:12:54 +0000

    "Open" and "Close" menu items added
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1235 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScalerRes/Resource.h
+++ DScalerRes/Resource.h
@@ -796,15 +796,17 @@
 #define IDM_SAVE_BY_INPUT               2188
 #define IDM_SAVE_BY_CHANNEL             2189
 #define IDM_VT_HUNGARIAN                2190
+#define IDM_OPEN_FILE                   2192
+#define IDM_CLOSE_FILE                  2193
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_3D_CONTROLS                1
+#define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        187
-#define _APS_NEXT_COMMAND_VALUE         2192
+#define _APS_NEXT_COMMAND_VALUE         2194
 #define _APS_NEXT_CONTROL_VALUE         1893
 #define _APS_NEXT_SYMED_VALUE           2192
 #endif

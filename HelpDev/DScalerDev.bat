91065de175b2b83ecccfeeca0ecebb4928d9b95b
adcockj <adcockj@users.sourceforge.net>
2001-12-06 13:53:14 +0000

    Updated config file with Torbjorn's suggestions
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1274 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- HelpDev/DScalerDev.bat
+++ HelpDev/DScalerDev.bat
@@ -4,7 +4,12 @@ REM The batch file uses the source file javadoc to
 REM generate a compressed help file.
 REM
 REM to use you must have both 
-REM doxygen.exe and hhc.exe in your path
+REM doxygen.exe, hhc.exe and dot.exe in your path
+REM 
+REM Get doxygen from
+REM Get dot.exe from http://www.graphviz.org/pub/graphviz/windows/
+REM        (You will also need cygwin1.dll)
+REM Get hhc.exe by installing htmlhelp from the service pack
 REM ************************************************************
 del /q .\html\*.*
 doxygen Dscaler.cfg

9d1d61e9e0e529c1c23cdf069c38948551231314
lindsey <lindsey@users.sourceforge.net>
2001-12-23 01:43:53 +0000

    Added Gradual Noise Filter
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1347 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ Plugins/FLT_GradualNoise/FLT_GradualNoise.asm
@@ -0,0 +1,155 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: FLT_GradualNoise.asm,v 1.1.1.1 2001-12-23 01:43:52 lindsey Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2000 Steven Grimm.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+// Revision 1.3  2001/07/13 16:13:33  adcockj
+// Added CVS tags and removed tabs
+//
+/////////////////////////////////////////////////////////////////////////////
+
+// This is the implementation of the noise filter described in FLT_GradualNoise.c
+
+#if defined( IS_SSE )
+#define MAINLOOP_LABEL DoNext8Bytes_SSE
+#else // IS_MMXEXT
+#define MAINLOOP_LABEL DoNext8Bytes_MMXEXT
+#endif
+
+#if defined( IS_SSE )
+long FilterGradualNoise_SSE( TDeinterlaceInfo *pInfo )
+#else // IS_MMXEXT
+long FilterGradualNoise_MMXEXT( TDeinterlaceInfo *pInfo )
+#endif
+{
+    BYTE*           pSource = NULL;
+    DWORD           ThisLine = pInfo->SourceRect.top/2;
+    const LONG      Cycles = pInfo->LineLength;
+    // Noise multiplier is (1/gNoiseReduction), in 0x10000 fixed point.
+    // This times the measured noise gives the weight given to the new pixel color
+    const DWORD     NoiseMultiplier = 0x10000/(gNoiseReduction);
+    const __int64   qwChromaMask = 0xFF00FF00FF00FF00;
+    const __int64   qwOnes = 0x0101010101010101;
+
+    BYTE*           pLast = NULL;
+    const DWORD     BottomLine = pInfo->SourceRect.bottom/2;        // Limit processing to displayed picture
+
+    // Need to have the current and next-to-previous fields to do the filtering.
+    if( (pInfo->PictureHistory[0] == NULL) || (pInfo->PictureHistory[2] == NULL) )
+    {
+        return 1000;
+    }
+
+    // Warning: Dense code ahead
+
+    pSource = pInfo->PictureHistory[0]->pData + (ThisLine * pInfo->InputPitch);
+    pLast = pInfo->PictureHistory[2]->pData + (ThisLine * pInfo->InputPitch);
+
+    for( ; ThisLine < BottomLine; ++ThisLine )
+    {
+
+        _asm 
+        {
+            mov ecx, Cycles
+
+            // Load pointers into normal registers
+            mov edi, dword ptr[pSource]
+            mov ebx, dword ptr[pLast]
+
+align 8
+MAINLOOP_LABEL:
+
+            movq mm0, qword ptr[edi]        // * mm0 = NewPixel
+            movq mm1, qword ptr[ebx]        // * mm1 = OldPixel
+
+            // Get the noise adjustment multiplier
+
+            movq mm2, mm0                   // mm2 = NewPixel
+            psadbw mm2, mm1                 // mm2 = Sum(|byte differences Old,New|)
+
+            // The Athlon doesn't have an unsigned MMX 16 bit multiply which returns the low word, so...
+#if defined(IS_SSE)
+            pmuludq mm2, NoiseMultiplier    // mm2 (low DWord) = Multiplier to move toward new pixel value
+#else // IS_MMXEXT
+            movd eax, mm2                   // eax = Sum(|byte differences Old, New|)
+            mul NoiseMultiplier             // eax = Multiplier to move toward new pixel value *overwrites edx*
+            movd mm2, eax                   // mm2 (low Dword) = Multiplier to move toward new pixel value
+#endif
+            movq mm3, mm2                   // mm3 = same
+            pxor mm6, mm6                   // mm6 = 0
+            pcmpgtw mm2, mm6                // mm2 = 0x.......FFFF.... if ignoring old pixel value
+            psrlq mm2, 16                   // mm2 (low word) = FFFF if ignoring old pixel value (else 0)
+            por mm3, mm2                    // mm3 = (low word) Adjusted amount to move toward new
+            pshufw mm2, mm3, 0x00           // * mm2 = (wordwise) adjusted multiplier to move toward new
+
+            movq mm4, mm1                   // mm4 = OldPixel
+            psubusb mm4, mm0                // mm4 = bytewise max(old - new, 0)
+            psubusb mm0, mm1                // mm0 = bytewise max (new - old, 0)
+            por mm4, mm0                    // * mm4 = bytewise |new - old|
+            pcmpeqb mm0, mm6                // * mm0 = bytewise on where old >= new
+            movq mm5, mm4                   // mm5 = bytewise |new - old|
+            pcmpeqb mm5, mm6                // mm5 bytewise on where |new - old| = 0
+            pandn mm5, qwOnes               // mm5 bytewise 1 where |new - old| != 0 (used for "round at least 1")
+
+            pcmpeqw mm6, mm6                // mm6 = all on
+            pcmpeqw mm6, mm2                // mm6 = all on iff multiplier = 0xFFFF (~= 1)
+            pand mm6, mm4                   // mm6 = bytewise |new - old| iff multiplier = 0xFFFF
+            pmaxub mm6, mm5                 // mm6 = same, corrected to a minimum of 1 where there is change
+
+            // Multiply the change by the determined compensation factor
+            movq mm7, mm4                   // mm7 = bytewise |new - old|
+            movq mm3, qwChromaMask
+            pand mm7, mm3                   // mm7 = bytewise |new - old| chroma
+            pmulhuw mm7, mm2                // mm7 = amount of chroma to add/subtract from old, with remainders
+            pand mm7, mm3                   // mm7 = amount of chroma to add/subtract from old
+            pandn mm3, mm4                  // mm3 = bytewise |new - old| luma
+            pmulhuw mm3, mm2                // mm3 = amount of luma to add/subtract from old
+            por mm7, mm3                    // mm7 = amount to add/subtract from old
+            pmaxub mm7, mm6                 // mm7 = corrected to to deal with the motion and round up to 1
+            
+            // Apply the calculated change
+            movq mm3, mm7                   // mm3 = same
+            pand mm7, mm0                   // mm7 = amount to subtract
+            psubusb mm1, mm7                // mm1 = Partially adjusted oldpixel
+            pandn mm0, mm3                  // mm0 = amount to add
+            paddusb mm1, mm0                // mm1 = Adjusted pixel value
+
+
+            movq qword ptr[edi], mm1
+
+            add edi, 8
+            add ebx, 8
+            sub ecx, 8
+            jnz MAINLOOP_LABEL
+        }
+
+        pSource += pInfo->InputPitch;
+        pLast += pInfo->InputPitch;
+    }
+
+    // clear out the MMX registers ready for doing floating point
+    // again
+    _asm
+    {
+        emms
+    }
+
+    return 1000;
+}
+
+#undef MAINLOOP_LABEL
---
+++ Plugins/FLT_GradualNoise/FLT_GradualNoise.c
@@ -0,0 +1,264 @@
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2001 Lindsey Dubb.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#include "windows.h"
+#include "DS_Filter.h"
+
+/////////////////////////////////////////////////////////////////////////////
+/*
+We all learn by experience, and your lesson this time is that you should
+never lose sight of the alternative.
+                                             -- Sherlock Holmes
+
+
+Gradual noise reduction filter.  This noise filter smooths out slight
+variations in pixels between adjacent frames.
+
+This algorithm is very similar to what Andrew Dowsey came up with in his "Adaptive
+Temporal Averaging" for his DirectShow filter.  The algorithms differ in 1) their
+block size,  2) their motion estimation (sum of absolute differences versus mean
+squared error), 3) rounding.
+
+
+The algorithm:
+
+This filter gets the sum of absolute differences between a four pixel
+horizontal block in the current image and the same block in the preceding
+frame.  This isn't the best local motion measure, but it's very fast due to
+the psadbw SSE instruction.
+
+This difference measure is used to determine the kind of averaging which will be
+conducted.  If it's more than the "noise reduction" parameter, motion is
+inferred.  In that case, we just use the new pixel values.  If it's less than the 
+noise reduction, we use the ratio of (difference/noise reduction) to determine the
+weighting of the old and new values.
+
+Somewhat more formally:
+
+  N = Sum_block(|oldByte - newByte|)
+  R = Noise Reduction parameter
+  M = (motion evidence) = 1     if N/R >= 1
+                          N/R   otherwise
+  Result pixel = (bytewise) oldPixel * (1 - M) + newPixel * M
+
+
+Rounding has a very significant effect on the algorithm.  In general, for
+computational reasons, values are rounded down.  An important exception 
+occurs when
+    M > 0 and oldPixel != newPixel
+but
+    oldPixel * (1 - M) + newPixel * M
+rounds to oldPixel.  In that case, the Result pixel is rounded to one toward
+the newPixel value.  This makes sure that very gradual variation is maintained.
+
+
+The rationale:
+
+This filter was designed to get rid of the artifacts caused by the use of
+a hard threshold in the Temporal Noise Filter.  The problems were:
+- Speckles:
+  Noise tends to follow something like a normal distribution. When an extreme
+  value comes up, the Temporal Noise Filter will conclude that it represents
+  motion.  When this happens in a stationary area where averaging is occuring, a
+  speckle is seen.
+- Color bias, and the inability to fade all the way to black:
+  These are both due to a bias in the averaging routine used in the filter.
+- Loss of color depth:
+  When there's just a little noise, the Temporal Noise Filter will completely
+  stabilize the colors.  This loses some information, as wavering between
+  nearby colors can indicate that the true value is between them.
+- Posterization:
+  One cause of posterization is loss of color depth described above. Another is
+  the approximate averaging on machines without an MMX averaging instruction.
+  The third cause is more complicated...
+  In an area of low contrast motion, it is possible for part of the area to
+  barely exceed the noise threshold while another part is just below it. As a
+  result, the second area will be averaged while the first is left alone.  This
+  causes an artificial boundary in the image.
+
+By using a gradual method rather than a hard threshold, those artifacts are
+affected so:
+- Speckles:
+  This is greatly improved.  Extreme values from the noise distribution are
+  partially averaged, so they stand out much less.  The blockwise motion
+  estimation also helps reduce the effect of speckles.  But really extreme
+  speckles (like from an old film) will look just as bad as before.
+- Color bias, and the inability to fade all the way to black:
+  This is solved.
+- Loss of color depth:
+  This is improved by the "round to one" part of the algorithm.  But there is
+  still a significant loss.
+- Posterization:
+  This is significantly improved.  In areas of low contrast motion, there's no
+  threshold value at which averaging stops, so there the artifactual borders are
+  less obvious.  There are still some borders, though, due to rounding and loss of 
+  color depth.
+
+With that stuff in mind, the goal was to make the filter as fast as possible.  As
+a result, it isn't any better at avoiding the most important noise reduction artifact:
+blurring.
+
+
+An alternate rationale:
+
+The above explanation sounds good, but this plugin is really a cut down version of
+a filter designed as a loose approximation of a more complicated model.
+
+Model -- Assume the image is a combination of "stationary" and "moving" pixels. Assume
+noise is distributed ~ N(0, n_sigma^2) around the "true" image value, and motion is
+distributed ~ N(0, m_sigma^2) around the previous "true" image value.  Noise
+occurs for all pixels regardless of whether they are moving. Assume the perceived
+image error at a pixel Q = ("true image" - displayed image)^2 . For the purpose of this
+plugin, make the questionable assumption that pixels never switch between {stationary,
+moving}, ignore edge effects, and ignore correlation between neighboring pixels.
+
+Then, as a function of the color difference, the variances, and the distribution of past
+estimates, you can find P(moving|Data) and P(stationary|Data) and calculate the pixel value
+which will minimize Q.
+
+That's not what this plugin does.  But for a reasonable range of m_sigma and n_sigma,
+when applied to a single pixel, the function is a passable approximation. One caveat: I
+haven't looked at the effect of the four pixel blocking.
+
+You can try to estimate n_sigma^2, m_sigma^2, and use temporal and spatial correlation to
+come up with a much better estimator of motion.  But that's a job for a different -- and
+slower -- plugin.
+
+
+Yowza! Look at all that text!
+
+
+A new artifact:
+
+The filter gives blocking artifacts when it's used at really high Noise Reduction values.
+These appear as vertical lines in areas of motion.
+
+
+Other notes:
+
+Don't use this and the temporal noise filter at the same time.  They're both using the
+same data to achieve the same goal, so it just doesn't make sense.  Combined, they give
+you the "comical posterization" filter.
+
+*/
+/////////////////////////////////////////////////////////////////////////////
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+/////////////////////////////////////////////////////////////////////////////
+
+__declspec(dllexport) FILTER_METHOD* GetFilterPluginInfo( long CpuFeatureFlags );
+BOOL WINAPI     _DllMainCRTStartup( HANDLE hInst, ULONG ul_reason_for_call, LPVOID lpReserved );
+long            FilterGradualNoise_SSE( TDeinterlaceInfo *pInfo );
+long            FilterGradualNoise_MMXEXT( TDeinterlaceInfo *pInfo );
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Begin plugin globals
+/////////////////////////////////////////////////////////////////////////////
+
+// Just one global: The maximum sum of absolute differences in a four pixel
+// block.  Above this, the old pixel values will be ignored.
+
+long gNoiseReduction = 40;
+
+
+FILTER_METHOD GradualNoiseMethod;
+
+SETTING FLT_GradualNoiseSettings[FLT_GNOISE_SETTING_LASTONE] =
+{
+    {
+        "Noise Reduction", SLIDER, 0, &gNoiseReduction,
+        40, 1, 100, 1, 1,
+        NULL,
+        "GradualNoiseFilter", "NoiseReduction", NULL,
+    },
+    {
+        "Enable Gradual Noise Filter", NOT_PRESENT, 0, &GradualNoiseMethod.bActive,
+        FALSE, 0, 1, 1, 1,
+        NULL,
+        "GradualNoiseFilter", "UseTemporalNoiseFilter", NULL,
+    },
+};
+
+FILTER_METHOD GradualNoiseMethod =
+{
+    sizeof(FILTER_METHOD),
+    FILTER_CURRENT_VERSION,
+    DEINTERLACE_INFO_CURRENT_VERSION,
+    "Gradual Noise Filter",                 // Displayed name
+    NULL,                                   // Use same name in menu
+    FALSE,                                  // Not initially active
+    TRUE,                                   // Call on input so pulldown/deinterlacing can benefit
+    FilterGradualNoise_MMXEXT,              // Algorithm to use (really decided by GetFilterPluginInfo)
+    0,                                      // Menu: assign automatically
+    FALSE,                                  // Does not run if we've run out of time
+    NULL,                                   // No initialization procedure
+    NULL,                                   // No deallocation
+    NULL,                                   // Menu handle (for DScaler)
+    FLT_SNOISE_SETTING_LASTONE,             // Number of settings            
+    FLT_GradualNoiseSettings,
+    WM_FLT_GNOISE_GETVALUE - WM_USER,       // Settings offset
+    TRUE,                                   // Can handle interlaced material
+    2,                                      // Requires field before last
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Main code (included from FLT_GNoise.asm)
+/////////////////////////////////////////////////////////////////////////////
+
+#define IS_SSE
+#include "FLT_GradualNoise.asm"
+#undef IS_SSE
+
+#define IS_MMXEXT
+#include "FLT_GradualNoise.asm"
+#undef IS_MMXEXT
+
+
+////////////////////////////////////////////////////////////////////////////
+// Start of utility code
+/////////////////////////////////////////////////////////////////////////////
+
+__declspec(dllexport) FILTER_METHOD* GetFilterPluginInfo( long CpuFeatureFlags )
+{
+    if( CpuFeatureFlags & FEATURE_SSE )
+    {
+        GradualNoiseMethod.pfnAlgorithm = FilterGradualNoise_SSE;
+    }
+    else if( CpuFeatureFlags & FEATURE_MMXEXT )
+    {
+        GradualNoiseMethod.pfnAlgorithm = FilterGradualNoise_MMXEXT;
+    }
+    else
+    {
+        GradualNoiseMethod.pfnAlgorithm = NULL;
+    }
+    return &GradualNoiseMethod;
+}
+
+BOOL WINAPI _DllMainCRTStartup( HANDLE hInst, ULONG ul_reason_for_call, LPVOID lpReserved )
+{
+    return TRUE;
+}
+

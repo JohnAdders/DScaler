2f83190e7794c6251db5f89891e9ef7ac933f3b4
tobbej <tobbej@users.sourceforge.net>
2002-02-03 10:33:41 +0000

    fixed spelling error
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1457 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/stdafx.h
+++ DScaler/stdafx.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: stdafx.h,v 1.13 2001-12-17 19:54:20 tobbej Exp $
+// $Id: stdafx.h,v 1.14 2002-02-03 10:33:41 tobbej Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2000 John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -18,6 +18,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.13  2001/12/17 19:54:20  tobbej
+// added a comment about the dshow define
+//
 // Revision 1.12  2001/12/14 13:59:34  adcockj
 // Fix to remove dependancy on dshow.h if not required
 //
@@ -56,7 +59,7 @@
 
 #include <atlbase.h>
 
-//uncommen the folowing line if you want to try the experimental direct show support
+//uncomment the folowing line if you want to try the experimental direct show support
 //#define WANT_DSHOW_SUPPORT 1
 #ifdef WANT_DSHOW_SUPPORT
     #include <dshow.h>

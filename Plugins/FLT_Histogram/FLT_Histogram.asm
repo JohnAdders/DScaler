8573079d6cfae4b937a54d0e4705f8cc53004efd
lindsey <lindsey@users.sourceforge.net>
2002-02-16 00:15:43 +0000

    Added histogram filter
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1540 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ Plugins/FLT_Histogram/FLT_Histogram.asm
@@ -0,0 +1,214 @@
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Lindsey Dubb.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+/////////////////////////////////////////////////////////////////////////////
+
+// Processor specific averaging:
+// Set destMM to average of destMM and sourceMM
+// Note that this is a somewhat unconventional averaging function: It rounds toward
+// the first operand if it is (even and larger) or (odd and smaller).  This is faster
+// and just as effective here as "round toward even."
+// Explanation of the MMX version: 1 is added to the source pixel if it is odd (and != 255)
+// Then half the (adjusted) source pixel (rounding down -- which is effectively the same as
+// rounding the unadjusted pixel up unless source == 255) is added to half the destination
+// pixel (also rounding down). This gives the same result as the much faster and less 
+// complicated versions for other processors
+
+// tempMM is changed 
+
+#undef AVERAGE
+#if defined(IS_SSE)
+#define AVERAGE(destMM, sourceMM, tempMM, noLowBitsMask) __asm \
+    { \
+    __asm pand destMM, noLowBitsMask \
+    __asm pavgb destMM, sourceMM \
+    }
+#elif defined(IS_3DNOW)
+#define AVERAGE(destMM, sourceMM, tempMM, noLowBitsMask) __asm \
+    { \
+    __asm pand destMM, noLowBitsMask \
+    __asm pavgusb destMM, sourceMM \
+    }
+#else
+#define AVERAGE(destMM, sourceMM, tempMM, noLowBitsMask) __asm \
+    { \
+    __asm movq tempMM, noLowBitsMask \
+    __asm pandn tempMM, sourceMM \
+    __asm paddusb tempMM, sourceMM \
+    __asm pand tempMM, noLowBitsMask \
+    __asm psrlw tempMM, 1 \
+    __asm pand destMM, noLowBitsMask \
+    __asm psrlw destMM, 1 \
+    __asm paddusb destMM, tempMM \
+    }
+#endif // processor specific averaging routine
+
+
+#undef MAINLOOP_LABEL
+#if defined( IS_SSE )
+#define MAINLOOP_LABEL DoNext8Bytes_SSE
+#elif defined( IS_3DNOW )
+#define MAINLOOP_LABEL DoNext8Bytes_3DNOW
+#else // IS_MMX
+#define MAINLOOP_LABEL DoNext8Bytes_MMX
+#endif
+
+
+#if defined( IS_SSE )
+LONG GatherHistogram_SSE( TDeinterlaceInfo* pInfo )
+#elif defined( IS_3DNOW )
+LONG GatherHistogram_3DNOW( TDeinterlaceInfo* pInfo )
+#else   // IS_MMX
+LONG GatherHistogram_MMX( TDeinterlaceInfo* pInfo )
+#endif
+{
+    const __int64   qwNoLowBits = 0xFEFEFEFEFEFEFEFE;
+    const DWORD     YByte = 0x000000FF;
+    const DWORD     HighYByte = 0x00FF0000;
+    const DWORD     UByte = 0xFF000000;
+    const DWORD     VByte = 0x0000FF00;
+    DWORD*          pLocalYHistogram = NULL;
+    DWORD*          pLocalUHistogram = NULL;
+    DWORD*          pLocalVHistogram = NULL;
+    BYTE*           Pixels = NULL;
+    BYTE*           UpPixels = NULL;
+    DWORD           Index = 0;
+    DWORD           OldSI = 0;
+    const DWORD     Cycles = pInfo->LineLength / 8;
+
+
+    // If we're not using a comb filter, we just average the line with itself.  This seems comically
+    // inefficient, but appears to be just as fast.
+
+    UpPixels = pInfo->PictureHistory[0]->pData;
+    if( gUseComb == TRUE )
+    {
+        Pixels = UpPixels + pInfo->InputPitch;
+    }
+    else
+    {
+        Pixels = UpPixels;
+    }
+
+    pLocalYHistogram = gpYHistogram;
+    pLocalUHistogram = gpUHistogram;
+    pLocalVHistogram = gpVHistogram;
+
+    for (Index = 1; Index < (DWORD) pInfo->FieldHeight; ++Index)
+    {
+        _asm
+        {
+            mov     OldSI, esi
+            mov     ecx, Cycles
+            mov     edi, dword ptr[Pixels]
+            mov     esi, dword ptr[UpPixels]
+
+            MAINLOOP_LABEL:
+
+                movq    mm0, qword ptr[edi]                 // mm0 = four pixels
+                movq    mm1, qword ptr[esi]                 // mm1 = four pixels from previous line
+
+                AVERAGE(mm0, mm1, mm2, qwNoLowBits)         // * mm0 = vertical average of pixels
+
+                movd    edx, mm0                            // * edx = low two pixels
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalYHistogram]    // ebx points to Y histogram
+                and     eax, YByte                          // eax = Y value of low pixel
+                shl     eax, 2                              // eax = sizeof(DWORD)*(Y value)
+                add     ebx, eax                            // ebx = pointer to corresponding Y histogram value
+                inc     dword ptr[ebx]                      // increment Y histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalYHistogram]    // ebx points to Y histogram
+                and     eax, HighYByte                      // eax = Y value of high pixel
+                shr     eax, 14                             // eax = sizeof(DWORD)*(Y value)
+                add     ebx, eax                            // ebx = pointer to corresponding Y histogram value
+                inc     dword ptr[ebx]                      // increment Y histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalUHistogram]    // ebx points to U histogram
+                and     eax, UByte                          // eax = U value of two pixels
+                shr     eax, 22                             // eax = sizeof(DWORD)*(U value)
+                add     ebx, eax                            // ebx = pointer to corresponding U histogram value
+                inc     dword ptr[ebx]                      // increment U histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalVHistogram]    // ebx points to V histogram
+                and     eax, VByte                          // eax = V value of two pixels
+                shr     eax, 6                              // eax = sizeof(DWORD)*(V value)
+                add     ebx, eax                            // ebx = pointer to corresponding V histogram value
+                inc     dword ptr[ebx]                      // increment V histogram
+
+                psrlq   mm0, 32                             // mm0 = high two pixels in low dword
+#if defined( IS_SSE )
+                prefetchnta[edi + PREFETCH_STRIDE]
+#elif defined( IS_3DNOW )
+                prefetch[edi + PREFETCH_STRIDE]
+#endif
+                movd    edx, mm0                            // * edx = high two pixels
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalYHistogram]    // ebx points to Y histogram
+                and     eax, YByte                          // eax = Y value of low pixel
+                shl     eax, 2                              // eax = sizeof(DWORD)*(Y value)
+                add     ebx, eax                            // ebx = pointer to corresponding Y histogram value
+                inc     dword ptr[ebx]                      // increment Y histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalYHistogram]    // ebx points to Y histogram
+                and     eax, HighYByte                      // eax = Y value of high pixel
+                shr     eax, 14                             // eax = sizeof(DWORD)*(Y value)
+                add     ebx, eax                            // ebx = pointer to corresponding Y histogram value
+                inc     dword ptr[ebx]                      // increment Y histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalUHistogram]    // ebx points to U histogram
+                and     eax, UByte                          // eax = U value of two pixels
+                shr     eax, 22                             // eax = sizeof(DWORD)*(U value)
+                add     ebx, eax                            // ebx = pointer to corresponding U histogram value
+                inc     dword ptr[ebx]                      // increment U histogram
+
+                mov     eax, edx                            // eax = pixels
+                mov     ebx, dword ptr[pLocalVHistogram]    // ebx points to V histogram
+                and     eax, VByte                          // eax = V value of two pixels
+                shr     eax, 6                              // eax = sizeof(DWORD)*(V value)
+                add     ebx, eax                            // ebx = pointer to corresponding V histogram value
+                inc     dword ptr[ebx]                      // increment V histogram
+
+                add     edi, 8
+                add     esi, 8
+
+            dec     ecx
+            jnz     MAINLOOP_LABEL
+
+            mov     esi, OldSI
+       }
+        Pixels += pInfo->InputPitch;
+        UpPixels += pInfo->InputPitch;
+    }
+    _asm
+    {
+#if defined( IS_3DNOW )
+        femms
+#else
+        emms
+#endif
+    }
+
+    return 1000;
+}
---
+++ Plugins/FLT_Histogram/FLT_Histogram.c
@@ -0,0 +1,307 @@
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Lindsey Dubb.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+/////////////////////////////////////////////////////////////////////////////
+
+/*
+Some are red.  And some are blue.
+Some are old.  And some are new.
+
+Some are sad.
+
+And some are glad.
+
+And some are very, very bad.
+
+Why are they sad and glad and bad?
+I do not know.  Go ask your dad.
+
+                           -- Dr. Seuss
+
+Histogram filter.  This diagnostic filter counts the frequency of pixels with
+a certain Y, U, or V value, and reports the results in an on screen (logarithmic)
+graph.
+
+There isn't a whole lot to it.  It could definitely be more efficient,
+but I don't consider efficiency to be a very high priority, here.
+
+*/
+
+#include <math.h>
+
+#include "windows.h"
+#include "DS_Filter.h"
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Constants
+/////////////////////////////////////////////////////////////////////////////
+
+// This might need to be tuned for other processors.
+// Then again, it doesn't seem to matter much what I set it to on my Athlon
+// so long as it's between 64 and about 300
+
+#define PREFETCH_STRIDE                         128
+
+// Colors used in the histogram
+
+#define Y_DOT_COLOR                             0x7FFF7FFF
+#define U_DOT_COLOR                             0xFFFF7FFF
+#define V_DOT_COLOR                             0x7FFFFFFF
+
+// Vertical scale of the histogram
+
+#define HISTOGRAM_SCALE                         16.0
+
+// Vertical space above and below the histogram
+
+#define MARGIN                                  20
+
+// Well, I could have done this with three checkboxes, but this corresponds more closely
+// to the way I expect the filter will be used.  Besides, it prevents the silly situation
+// of having the filter enabled but nothing displayed.
+
+typedef enum
+{
+    MODE_Y,
+    MODE_U,
+    MODE_V,
+    MODE_UV,
+    MODE_YUV,
+    MODE_LASTONE,
+} eMODE;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Function prototypes
+/////////////////////////////////////////////////////////////////////////////
+
+__declspec(dllexport)   FILTER_METHOD* GetFilterPluginInfo( long CpuFeatureFlags );
+
+void                    ShowHistogram( DWORD* pHistogram, TDeinterlaceInfo* pInfo, DWORD Color );
+LONG __cdecl            DispatchHistogram( TDeinterlaceInfo* pInfo );
+
+LONG                    GatherHistogram_SSE( TDeinterlaceInfo* pInfo );
+LONG                    GatherHistogram_3DNOW( TDeinterlaceInfo* pInfo );
+LONG                    GatherHistogram_MMX( TDeinterlaceInfo* pInfo );
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Globals
+/////////////////////////////////////////////////////////////////////////////
+
+FILTER_METHOD HistogramMethod;
+
+
+// The histograms
+
+DWORD   gpYHistogram[256];
+DWORD   gpUHistogram[256];
+DWORD   gpVHistogram[256];
+
+
+// Which of the histograms should be displayed?
+
+eMODE   gMode = MODE_YUV;
+
+// Should we average two scanlines to reduce color crosstalk artifacts?
+
+BOOLEAN gUseComb = TRUE;
+
+
+// Store CPU type for use in the dispatcher routine
+
+LONG    gCpuFeatureFlags = 0;
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Settings
+/////////////////////////////////////////////////////////////////////////////
+
+LPCSTR ModeList[] =
+{
+    "Brightness (Y)",
+    "U (Red) Color",
+    "V (Blue) Color",
+    "Colors",
+    "All pixel components"
+};
+
+
+SETTING FLT_HistogramSettings[FLT_HISTOGRAM_SETTING_LASTONE] =
+{
+    {
+        "Display Mode", ITEMFROMLIST, 0, (LONG*)&gMode,
+        MODE_YUV, MODE_Y, MODE_LASTONE -1, 1, 1,
+        ModeList,
+        "HistogramFilter", "DisplayMode", NULL,
+    },
+    {
+        "Use Histogram Filter", ONOFF, 0, &(HistogramMethod.bActive),
+        FALSE, 0, 1, 1, 1,
+        NULL,
+        "HistogramFilter", "UseHistogramFilter", NULL,
+    },
+    {
+        "Use Comb Filter", ONOFF, 0, (LONG*)&gUseComb,
+        TRUE, 0, 1, 1, 1,
+        NULL,
+        "HistogramFilter", "UseCombFilter", NULL,
+    },
+};
+
+
+FILTER_METHOD HistogramMethod =
+{
+    sizeof(FILTER_METHOD),
+    FILTER_CURRENT_VERSION,
+    DEINTERLACE_INFO_CURRENT_VERSION,
+    "Histogram Filter",
+    "Histogram",
+    FALSE,
+    TRUE,
+    DispatchHistogram, 
+    0,
+    TRUE,
+    NULL,
+    NULL,
+    NULL,
+    FLT_HISTOGRAM_SETTING_LASTONE,
+    FLT_HistogramSettings,
+    WM_FLT_HISTOGRAM_GETVALUE - WM_USER,
+    TRUE,
+    1,
+};
+
+
+/////////////////////////////////////////////////////////////////////////////
+// Main code
+/////////////////////////////////////////////////////////////////////////////
+
+// The histogram gathering routine is included from FLT_Histogram.asm to allow
+// some processor specific optimizations
+
+#define IS_SSE
+#include "FLT_Histogram.asm"
+#undef IS_SSE
+
+#define IS_3DNOW
+#include "FLT_Histogram.asm"
+#undef IS_3DNOW
+
+#define IS_MMX
+#include "FLT_Histogram.asm"
+#undef IS_MMX
+
+
+// Save the CPU type for use in the dispatcher routine
+
+__declspec(dllexport) FILTER_METHOD* GetFilterPluginInfo( long CpuFeatureFlags )
+{
+    gCpuFeatureFlags = CpuFeatureFlags;
+    return &HistogramMethod;
+}
+
+
+// Traffic organizer -- Run the appropriate version of the data
+// gatherer, then call the appropriate display routines.
+
+LONG __cdecl DispatchHistogram( TDeinterlaceInfo* pInfo )
+{
+    DWORD       Index = 0;
+    LONG        FilterResult = 0;
+    
+
+    if( pInfo->PictureHistory[0] == NULL )
+    {
+        return 1000;
+    }
+
+    // Clear histograms
+    for( Index = 0; Index < 256; ++Index )
+    {
+        gpYHistogram[Index] = 0;
+        gpUHistogram[Index] = 0;
+        gpVHistogram[Index] = 0;
+    }
+
+    if( (gCpuFeatureFlags & FEATURE_SSE) || (gCpuFeatureFlags & FEATURE_MMXEXT) )
+    {
+        FilterResult = GatherHistogram_SSE(pInfo);
+    }
+    else if( gCpuFeatureFlags & FEATURE_3DNOW )
+    {
+        FilterResult = GatherHistogram_3DNOW(pInfo);
+    }
+    else  // MMX version
+    {
+        FilterResult = GatherHistogram_MMX(pInfo);
+    }
+
+
+    if( (gMode == MODE_Y) || (gMode == MODE_YUV) )
+    {
+        ShowHistogram(gpYHistogram, pInfo, Y_DOT_COLOR);
+    }
+    if( (gMode == MODE_U) || (gMode == MODE_UV) || (gMode == MODE_YUV) )
+    {
+        ShowHistogram(gpUHistogram, pInfo, U_DOT_COLOR);
+    }
+    if( (gMode == MODE_V) || (gMode == MODE_UV) || (gMode == MODE_YUV) )
+    {
+        ShowHistogram(gpVHistogram, pInfo, V_DOT_COLOR);
+    }
+
+    return FilterResult;
+}
+
+
+// Show just one of the color component histograms
+
+void ShowHistogram( DWORD* pHistogram, TDeinterlaceInfo* pInfo, DWORD Color )
+{
+    DWORD       Index = 0;
+    DWORD       LeftOffset = 0;
+
+    if( pInfo->FrameWidth > 512 )
+    {
+        LeftOffset = ((pInfo->FrameWidth - 512)/4);
+    }
+
+    for( ; (Index < 256) && (Index * 2 < (DWORD) pInfo->FrameWidth); ++Index )
+    {
+        DOUBLE      ScaledValue = 0.0;
+        DWORD       DWordScaledValue = 0;
+        DWORD       BottomLine = pInfo->FieldHeight - MARGIN;
+        
+        ScaledValue = HISTOGRAM_SCALE*log(1.0 + pHistogram[Index]);
+        DWordScaledValue = (DWORD) (ScaledValue + 1.00001);
+        if( DWordScaledValue < (DWORD) (pInfo->FieldHeight - MARGIN*2) )
+        {
+            *(DWORD*)(pInfo->PictureHistory[0]->pData + (BottomLine - DWordScaledValue) * pInfo->InputPitch + (Index+LeftOffset)*4) = Color;
+        }
+        else
+        {
+            *(DWORD*)(pInfo->PictureHistory[0]->pData + (MARGIN * pInfo->InputPitch) + (Index+LeftOffset)*4) = Color;
+        }
+
+        // Show dots every other pixel to make measurement a little easier.
+        // Yeah, this step is done three times if you're showing all three color components
+        *(WORD*)(pInfo->PictureHistory[0]->pData + (BottomLine * pInfo->InputPitch) + (Index+LeftOffset)*4) = 0x00FF;
+    }
+
+    return;
+}

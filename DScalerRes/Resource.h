a0dcfe765fe05a6b88ce0fc065acd25e67e7e658
tobbej <tobbej@users.sourceforge.net>
2002-02-19 09:25:18 +0000

    fixed resource symbols with same numbers
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1564 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScalerRes/Resource.h
+++ DScalerRes/Resource.h
@@ -881,9 +881,7 @@
 #define IDM_DSVIDEO_STANDARD_MAX        2333
 #define IDM_DSHOW_FILTER_0              2334
 #define IDM_DSHOW_FILTER_1              2335
-#define IDM_FINDLOCK_PULL               2335
 #define IDM_DSHOW_FILTER_2              2336
-#define IDM_USE_DSCALER_OVERLAY         2336
 #define IDM_DSHOW_FILTER_3              2337
 #define IDM_DSHOW_FILTER_4              2338
 #define IDM_DSHOW_FILTER_5              2339
@@ -902,6 +900,8 @@
 #define IDM_DSHOW_FILTER_18             2352
 #define IDM_DSHOW_FILTER_19             2353
 #define IDM_DSHOW_FILTER_MAX            2353
+#define IDM_FINDLOCK_PULL               2354
+#define IDM_USE_DSCALER_OVERLAY         2355
 #define IDC_STATIC                      -1
 #define IDM_POPUP_SOURCES               65535
 #define IDM_POPUP_HOLDER                65535
@@ -918,13 +918,13 @@
 #define IDM_HOLDER                      65535
 
 // Next default values for new objects
-// 
+//
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        188
 #define _APS_NEXT_COMMAND_VALUE         2337
 #define _APS_NEXT_CONTROL_VALUE         1895
-#define _APS_NEXT_SYMED_VALUE           2354
+#define _APS_NEXT_SYMED_VALUE           2356
 #endif
 #endif

e7f536b1a82f45352771b6d88bec377b9f9ac2da
Rob Muller <robmuller@users.sourceforge.net>
2002-03-11 22:28:38 +0000

    Set cursor to hand cursor when over a URL.
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1639 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/Dialogs.cpp
+++ DScaler/Dialogs.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: Dialogs.cpp,v 1.16 2002-03-04 20:49:36 adcockj Exp $
+// $Id: Dialogs.cpp,v 1.17 2002-03-11 22:28:38 robmuller Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2000 John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -41,6 +41,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.16  2002/03/04 20:49:36  adcockj
+// About box changes
+//
 // Revision 1.15  2001/11/23 10:49:16  adcockj
 // Move resource includes back to top of files to avoid need to rebuild all
 //
@@ -167,6 +170,9 @@ BOOL APIENTRY AboutProc(HWND hDlg, UINT message, UINT wParam, LONG lParam)
                     }
                 }
             } // if (dwVerInfoSize)
+
+            SetClassLong(GetDlgItem(hDlg, IDC_LINK), GCL_HCURSOR, (long) hCursorHand);
+            SetClassLong(GetDlgItem(hDlg, IDC_LINK2), GCL_HCURSOR, (long) hCursorHand);
         break;
     case WM_COMMAND:
         switch(LOWORD(wParam))

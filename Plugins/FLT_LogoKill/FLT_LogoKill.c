e8c5d179d96059a94986e5b941f456fcd0efaf0a
adcockj <adcockj@users.sourceforge.net>
2002-04-30 18:17:06 +0000

    New weighted logo killer modes from Jochen Trenner
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1738 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Plugins/FLT_LogoKill/FLT_LogoKill.c
+++ Plugins/FLT_LogoKill/FLT_LogoKill.c
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: FLT_LogoKill.c,v 1.9 2001-12-15 17:50:47 adcockj Exp $
+// $Id: FLT_LogoKill.c,v 1.10 2002-04-30 18:17:06 adcockj Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2001 John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -21,6 +21,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.9  2001/12/15 17:50:47  adcockj
+// Fix for zero width bug
+//
 // Revision 1.8  2001/11/26 15:27:19  adcockj
 // Changed filter structure
 //
@@ -50,6 +53,8 @@ typedef enum
     MODE_GREY,
     MODE_MAX,
     MODE_DYNAMIC_MAX,
+    MODE_WEIGHTED_C,
+    MODE_WEIGHTED_ASM,
     MODE_LASTONE,
 } eMODE;
 
@@ -59,6 +64,9 @@ LPCSTR ModeList[] =
 {
     "Grey",
     "Limit To Max Value",
+    "Dynamic Max",
+    "Weighted Average (asm)",
+    "Weighted Average (c)",
     "Dynamic Max",
 };
 
@@ -214,6 +222,233 @@ long LogoKiller(TDeinterlaceInfo* pInfo)
             jnz LOOP_MAX_OUTER
         }
         break;
+    // weighted average mode supllied by
+    // Jochen Trenner
+    case MODE_WEIGHTED_C:
+		{
+			int i,j;
+			long Width2;
+			long Mul1, Mul2, Mul3, Mul4;
+			long Weight1, Weight2;
+			long up1, up2, up3, up4, 
+				down1, down2, down3, down4,
+				left1, left2, left3, left4,
+				right1, right2, right3, right4;
+			long	ipo1, ipo2, ipo3, ipo4;
+			BYTE* lpOverlay_Pointer;
+			BYTE* lpCurrent_Pointer;
+			int top_pitch;
+
+			Width2=Width*2;
+			top_pitch=Pitch*Top;
+			lpOverlay_Pointer=lpOverlay+top_pitch;
+
+			for (i=0;i<Height;i++)
+			{
+				left1=*(lpOverlay_Pointer+i*Pitch);
+				left2=*(lpOverlay_Pointer+i*Pitch+1);
+				left3=*(lpOverlay_Pointer+i*Pitch+2);
+				left4=*(lpOverlay_Pointer+i*Pitch+3);
+				right1=*(lpOverlay_Pointer+i*Pitch+Width2*4);
+				right2=*(lpOverlay_Pointer+i*Pitch+Width2*4+1);
+				right3=*(lpOverlay_Pointer+i*Pitch+Width2*4+2);
+				right4=*(lpOverlay_Pointer+i*Pitch+Width2*4+3);
+				Mul1=abs(Height-1-i);
+				Mul2=i;
+				Weight1=abs(((Height-1)/2)-i);
+				++Weight1;
+				for(j=0;j<Width2;j++)
+				{
+					Weight2=abs(Width-1-j);
+					++Weight2;
+					Mul3=abs(Width2-1-j);
+					Mul4=j;
+					up1=*(lpOverlay_Pointer+j*4);
+					up2=*(lpOverlay_Pointer+j*4+1);
+					up3=*(lpOverlay_Pointer+j*4+2);
+					up4=*(lpOverlay_Pointer+j*4+3);
+
+					down1=*(lpOverlay_Pointer+Pitch*Height+j*4);
+					down2=*(lpOverlay_Pointer+Pitch*Height+j*4+1);
+					down3=*(lpOverlay_Pointer+Pitch*Height+j*4+2);
+					down4=*(lpOverlay_Pointer+Pitch*Height+j*4+3);
+					lpCurrent_Pointer=lpOverlay_Pointer+i*Pitch+j*4;
+
+					ipo1=(((((Mul1*up1)+(Mul2*down1))*Weight1/(Mul1+Mul2))+(((Mul3*left1)+Mul4*right1)*Weight2/(Mul3+Mul4)))/(Weight1+Weight2));
+					ipo2=(((((Mul1*up2)+(Mul2*down2))*Weight1/(Mul1+Mul2))+(((Mul3*left2)+Mul4*right2)*Weight2/(Mul3+Mul4)))/(Weight1+Weight2));
+					ipo3=(((((Mul1*up3)+(Mul2*down3))*Weight1/(Mul1+Mul2))+(((Mul3*left3)+Mul4*right3)*Weight2/(Mul3+Mul4)))/(Weight1+Weight2));
+					ipo4=(((((Mul1*up4)+(Mul2*down4))*Weight1/(Mul1+Mul2))+(((Mul3*left4)+Mul4*right4)*Weight2/(Mul3+Mul4)))/(Weight1+Weight2));
+
+					if (i>Height/3 && i<2*Height/3)
+					{
+						ipo1=(2*ipo1+up1+down1)/4;
+						ipo2=(2*ipo2+up2+down2)/4;
+						ipo3=(2*ipo3+up3+down3)/4;
+						ipo4=(2*ipo4+up4+down4)/4;
+
+					}
+
+
+
+					*lpCurrent_Pointer=(BYTE)ipo1;
+					*(lpCurrent_Pointer+1)=(BYTE)ipo2;
+					*(lpCurrent_Pointer+2)=(BYTE)ipo3;
+					*(lpCurrent_Pointer+3)=(BYTE)ipo4;
+				}
+			}
+
+        }
+        break;
+    // weighted average mode supplied by
+    // Jochen Trenner
+    case MODE_WEIGHTED_ASM:
+		{
+			int Width_third=Width/3;
+			int Height_third=Height/3;
+			int Height_2third=Height/3*2;
+        _asm
+        {
+            mov edi, lpOverlay
+            mov ebx, Pitch
+            mov eax, Top
+            imul eax, ebx
+            add edi, eax
+            mov eax, Height
+            LOOP_JT1_OUTER:
+            mov edx, edi
+            movq mm0, qword ptr[edx]
+			add edx, Width
+			add edx, Width
+			add edx, Width
+			add edx, Width
+			add edx, Width
+			add edx, Width
+			add edx, Width
+			add edx, Width
+            movq mm1, qword ptr[edx]
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			sub edx, Width
+			movq mm2, mm0
+			pavgb mm2, mm1
+            mov ecx, Width_third
+
+            LOOP_JT1_LEFT:
+			push eax
+			push ecx
+			mov ecx, Height
+			sub ecx, eax
+			imul ecx, ebx
+			sub edx, ecx
+			movq mm3, qword ptr[edx]
+			add edx, ecx
+			imul eax, ebx
+			add edx, eax
+			movq mm4, qword ptr[edx]
+			sub edx, eax
+			pop ecx
+			pop eax
+			cmp eax, Height_2third
+			jge LA
+			cmp eax, Height_third
+			jge LM
+			movq mm5, mm4
+			jmp LEFT_AVG
+            LA:			
+            movq mm5, mm3
+			jmp LEFT_AVG
+            LM:
+            pavgb mm4, mm3
+			movq mm5, mm4
+            LEFT_AVG:	
+            pavgb mm5, mm0
+            movq qword ptr[edx], mm5
+            add edx, 8
+            dec ecx
+            jnz LOOP_JT1_LEFT
+
+            mov ecx, Width_third
+            LOOP_JT1_MIDDLE:
+			push eax
+			push ecx
+			mov ecx, Height
+			sub ecx, eax
+			imul ecx, ebx
+			sub edx, ecx
+			movq mm3, qword ptr[edx]
+			add edx, ecx
+			imul eax, ebx
+			add edx, eax
+			movq mm4, qword ptr[edx]
+			sub edx, eax
+			pop ecx
+			pop eax
+			cmp eax, Height_2third
+			jge MA
+			cmp eax, Height_third
+			jge MM
+			movq mm5, mm4
+			jmp MIDDLE_AVG
+            MA:
+            movq mm5, mm3
+			jmp MIDDLE_AVG
+            MM:
+            pavgb mm4, mm3
+			movq mm5, mm4
+            MIDDLE_AVG:	
+            pavgb mm5, mm2
+			movq qword ptr[edx], mm5
+            add edx, 8
+            dec ecx
+            jnz LOOP_JT1_MIDDLE
+
+            mov ecx, Width_third
+            
+            LOOP_JT1_RIGHT:
+			push eax
+			push ecx
+			mov ecx, Height
+			sub ecx, eax
+			imul ecx, ebx
+			sub edx, ecx
+			movq mm3, qword ptr[edx]
+			add edx, ecx
+			imul eax, ebx
+			add edx, eax
+			movq mm4, qword ptr[edx]
+			sub edx, eax
+			pop ecx
+			pop eax
+			cmp eax, Height_2third
+			jge RA
+			cmp eax, Height_third
+			jge RM
+			movq mm5, mm4
+			jmp RIGHT_AVG
+            RA:			
+            movq mm5, mm3
+			jmp RIGHT_AVG
+            RM:			
+            pavgb mm4, mm3
+			movq mm5, mm4
+            RIGHT_AVG:	
+            pavgb mm5, mm1
+            movq qword ptr[edx], mm5
+            add edx, 8
+            dec ecx
+            jnz LOOP_JT1_RIGHT
+
+            add edi, ebx
+            dec eax
+            jnz LOOP_JT1_OUTER
+        }
+        break;
+		}
     case MODE_GREY:
     default:
         _asm

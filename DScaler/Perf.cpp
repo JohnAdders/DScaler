1dbe63e4d23d1688da2e6453856ee9d0a539c019
Laurent Garnier <laurentg@users.sourceforge.net>
2002-05-02 16:43:32 +0000

    ceil replaced by floor
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1746 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/Perf.cpp
+++ DScaler/Perf.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: Perf.cpp,v 1.4 2002-01-31 13:02:46 robmuller Exp $
+// $Id: Perf.cpp,v 1.5 2002-05-02 16:43:32 laurentg Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2001 Laurent Garnier.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -18,6 +18,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.4  2002/01/31 13:02:46  robmuller
+// Improved accuracy and reliability of the performance statistics.
+//
 // Revision 1.3  2001/12/16 18:40:28  laurentg
 // Reset statistics
 //
@@ -351,11 +354,17 @@ double CPerf::GetAverageUsedFields()
 
 int CPerf::GetDroppedFieldsLastSecond()
 {
-    return (int)ceil(m_DroppedFieldsLastSec - 0.5);
+    // WARNING http://support.intel.com/support/processors/flag/tech.htm
+    // Use of floor instead of ceil
+//    return (int)ceil(m_DroppedFieldsLastSec - 0.5);
+    return (int)floor(m_DroppedFieldsLastSec + 0.5);
 }
 
 int CPerf::GetUsedFieldsLastSecond()
 {
-    return (int)ceil(m_UsedFieldsLastSec - 0.5);
+    // WARNING http://support.intel.com/support/processors/flag/tech.htm
+    // Use of floor instead of ceil
+//    return (int)ceil(m_UsedFieldsLastSec - 0.5);
+    return (int)floor(m_UsedFieldsLastSec + 0.5);
 }
 

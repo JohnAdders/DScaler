62f50fadf4b8edb3687c0b07c3a8ac5be95679c4
Laurent Garnier <laurentg@users.sourceforge.net>
2002-05-06 15:29:58 +0000

    Key <i> to show source informations through OSD
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@1757 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScalerRes/Resource.h
+++ DScalerRes/Resource.h
@@ -932,6 +932,7 @@
 #define IDM_SETTINGS_FILTERSETTINGS     32771
 #define IDM_SOURCE_INITIAL              32772
 #define IDM_DSHOW_FILTERS               32773
+#define IDM_SHOW_INFOS                  32774
 #define IDC_STATIC                      -1
 #define IDM_POPUP_SOURCES               65535
 #define IDM_POPUP_HOLDER                65535
@@ -948,12 +949,12 @@
 #define IDM_HOLDER                      65535
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        200
-#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_COMMAND_VALUE         32775
 #define _APS_NEXT_CONTROL_VALUE         8000
 #define _APS_NEXT_SYMED_VALUE           2391
 #endif

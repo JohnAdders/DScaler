eb1eb5e4aa2e4997bbf4bcb1cb02a681756cd224
adcockj <adcockj@users.sourceforge.net>
2002-08-19 19:00:39 +0000

    This file automates the process of creating a new release
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2107 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ Install/MakeSfBuild.bat
@@ -0,0 +1,29 @@
+@echo off
+echo Dscaler make a sourceforge build batch file
+echo (c) John Adcock 2002
+if "%1" == "" goto usage
+cd ..\..
+md DScalerBuild%1
+cd DScalerBuild%1
+cvs -z3 -d:ext:adcockj@cvs.sf.net:/cvsroot/deinterlace co DScaler
+cd DScaler\Dscaler
+..\..\..\Dscaler\Release\verinc
+echo cvs commit -m Update Verion for release %1
+cd ..
+pause
+7z a -tzip ..\DScaler%1src.zip *.* -r
+cd DScaler
+call d:\PROGRA~1\MICROS~2\VC98\bin\vcvars32.bat
+msdev Dscaler.dsw /MAKE "All"
+cd ..\Help
+"d:\Program Files\HTML Help Workshop\hhc.exe" Dscaler.hhp
+cd ..\Install
+"e:\Program Files\Inno Setup 2\Compil32.exe" /cc DScaler.iss
+copy Output\Setup.exe ..\..\DScaler%1.exe
+cd ..\..
+del /f /q /s DScaler  
+rd /s /q Dscaler
+goto endofbatch
+:usage
+echo To use this program enter the build number as a parameter
+:endofbatch

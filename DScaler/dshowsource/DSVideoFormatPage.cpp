e06c8cd6431eab5c522240045995d50d191d515c
tobbej <tobbej@users.sourceforge.net>
2002-09-04 17:08:31 +0000

    new video format configuration dialog (resolution)
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2146 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ DScaler/dshowsource/DSVideoFormatPage.cpp
@@ -0,0 +1,412 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: DSVideoFormatPage.cpp,v 1.1 2002-09-04 17:08:31 tobbej Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Torbjörn Jansson.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file DSVideoFormatPage.cpp implementation file
+ */
+
+#include "stdafx.h"
+#include "..\dscaler.h"
+#include "DSVideoFormatPage.h"
+#include "Other.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
+/////////////////////////////////////////////////////////////////////////////
+// CDSVideoFormatPage dialog
+
+
+CDSVideoFormatPage::CDSVideoFormatPage(CString name,vector<CDShowGraph::CVideoFormat> &fmts,CSimpleSetting *pResolution)
+	: CTreeSettingsPage(name,CDSVideoFormatPage::IDD),
+	m_RealVideoFmt(fmts),
+	m_bInUpdateControlls(false),
+	m_pResolutionSetting(pResolution)
+{
+	//{{AFX_DATA_INIT(CDSVideoFormatPage)
+		// NOTE: the ClassWizard will add member initialization here
+	//}}AFX_DATA_INIT
+
+	m_VideoFmt=fmts;
+}
+
+
+void CDSVideoFormatPage::DoDataExchange(CDataExchange* pDX)
+{
+	CTreeSettingsPage::DoDataExchange(pDX);
+	//{{AFX_DATA_MAP(CDSVideoFormatPage)
+	DDX_Control(pDX, IDC_DSHOW_VIDEOFMTS_FMTLIST, m_ListBox);
+	DDX_Control(pDX, IDC_DSHOW_VIDEOFMTS_WIDTH_SPIN,m_SpinWidth);
+	DDX_Control(pDX, IDC_DSHOW_VIDEOFMTS_HEIGHT_SPIN,m_SpinHeight);
+	DDX_Control(pDX, IDC_DSHOW_VIDEOFMTS_YUY2,m_YUY2Check);
+	DDX_Control(pDX, IDC_DSHOW_VIDEOFMTS_FIELDFMT, m_SampleFormat);
+	//}}AFX_DATA_MAP
+}
+
+
+BEGIN_MESSAGE_MAP(CDSVideoFormatPage, CTreeSettingsPage)
+	//{{AFX_MSG_MAP(CDSVideoFormatPage)
+	ON_LBN_SELCHANGE(IDC_DSHOW_VIDEOFMTS_FMTLIST,OnVideoFmtSelChange)
+	ON_NOTIFY(UDN_DELTAPOS, IDC_DSHOW_VIDEOFMTS_WIDTH_SPIN, OnDeltaPosWidth)
+	ON_NOTIFY(UDN_DELTAPOS, IDC_DSHOW_VIDEOFMTS_HEIGHT_SPIN, OnDeltaPosHeight)
+	ON_EN_CHANGE(IDC_DSHOW_VIDEOFMTS_WIDTH,OnChangeWidth)
+	ON_EN_CHANGE(IDC_DSHOW_VIDEOFMTS_HEIGHT,OnChangeHeight)
+	ON_EN_CHANGE(IDC_DSHOW_VIDEOFMTS_NAME,OnChangeName)
+	ON_BN_CLICKED(IDC_DSHOW_VIDEOFMTS_YUY2,OnClickedYUY2)
+	ON_CBN_SELENDOK(IDC_DSHOW_VIDEOFMTS_FIELDFMT,OnSelEndOkFieldFmt)
+	ON_WM_CONTEXTMENU()
+	ON_BN_CLICKED(IDC_DSHOW_VIDEOFMTS_NEW,OnClickedNew)
+	ON_BN_CLICKED(IDC_DSHOW_VIDEOFMTS_DELETE,OnClickedDelete)
+	//}}AFX_MSG_MAP
+END_MESSAGE_MAP()
+
+
+BOOL CDSVideoFormatPage::OnInitDialog() 
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnInitDialog\n"),__FILE__,__LINE__);
+	CTreeSettingsPage::OnInitDialog();
+	
+	for(vector<CDShowGraph::CVideoFormat>::size_type index=0;index<m_VideoFmt.size();index++)
+	{
+		int pos=m_ListBox.AddString(m_VideoFmt[index].m_Name.c_str());
+		if(pos!=LB_ERR)
+		{
+			int err=m_ListBox.SetItemData(pos,index);
+			if(err==LB_ERR)
+			{
+				//oops can't set item data
+			}
+		}
+	}
+	
+	int cbindex=m_SampleFormat.AddString(_T("Auto"));
+	m_SampleFormat.SetItemData(cbindex,DSREND_FIELD_FORMAT_AUTO);
+	cbindex=m_SampleFormat.AddString(_T("Frame"));
+	m_SampleFormat.SetItemData(cbindex,DSREND_FIELD_FORMAT_FRAME);
+	cbindex=m_SampleFormat.AddString(_T("Field"));
+	m_SampleFormat.SetItemData(cbindex,DSREND_FIELD_FORMAT_FIELD);
+	
+	//select first entry
+	m_ListBox.SetCurSel(0);
+	UpdateControlls();
+
+	return TRUE;
+}
+
+void CDSVideoFormatPage::OnOK()
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnOK\n"),__FILE__,__LINE__);
+	
+	//update the resoltion setting so it points to the right entry in the new vector
+	long OldResolution=m_pResolutionSetting->GetValue();
+	long NewResolution=-1;
+	if(OldResolution>=0 && OldResolution<m_RealVideoFmt.size())
+	{
+		for(int i=0;i<m_VideoFmt.size();i++)
+		{
+			if(m_VideoFmt[i]==m_RealVideoFmt[OldResolution])
+			{
+				NewResolution=i;
+				break;
+			}
+		}
+	}
+
+	m_RealVideoFmt=m_VideoFmt;
+	m_pResolutionSetting->SetValue(NewResolution);
+}
+
+void CDSVideoFormatPage::OnVideoFmtSelChange()
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnVideoFmtSelChange\n"),__FILE__,__LINE__);
+	if(m_ListBox.GetCurSel()!=LB_ERR)
+	{
+		UpdateControlls();
+	}
+}
+
+void CDSVideoFormatPage::UpdateControlls()
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::UpdateControlls\n"),__FILE__,__LINE__);
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+	
+	m_bInUpdateControlls=true;
+	int CurSel=m_ListBox.GetCurSel();
+	if(CurSel!=LB_ERR)
+	{
+		DWORD pos=m_ListBox.GetItemData(CurSel);
+		SetDlgItemText(IDC_DSHOW_VIDEOFMTS_NAME,m_VideoFmt[pos].m_Name.c_str());
+		SetDlgItemInt(IDC_DSHOW_VIDEOFMTS_WIDTH,m_VideoFmt[pos].m_Width);
+		SetDlgItemInt(IDC_DSHOW_VIDEOFMTS_HEIGHT,m_VideoFmt[pos].m_Height);
+		m_YUY2Check.SetCheck(m_VideoFmt[pos].m_bForceYUY2 ? BST_CHECKED : BST_UNCHECKED);
+		m_SampleFormat.SetCurSel(m_VideoFmt[pos].m_FieldFmt);
+
+		///@todo update the range of the spinboxes with proper values
+		m_SpinWidth.SetRange(16,DSCALER_MAX_WIDTH);
+		m_SpinHeight.SetRange(2,DSCALER_MAX_HEIGHT);
+	}
+	m_bInUpdateControlls=false;
+}
+
+void CDSVideoFormatPage::GenerateName(int pos)
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::GenerateName\n"),__FILE__,__LINE__);
+	CString name;
+	name.Format("%ldx%ld",m_VideoFmt[pos].m_Width,m_VideoFmt[pos].m_Height);
+
+	if(m_VideoFmt[pos].m_bForceYUY2 || m_VideoFmt[pos].m_FieldFmt!=DSREND_FIELD_FORMAT_AUTO)
+	{
+		name+=" (";
+		if(m_VideoFmt[pos].m_bForceYUY2)
+		{
+			name+="YUY2";
+		}
+		
+		if(m_VideoFmt[pos].m_bForceYUY2 && m_VideoFmt[pos].m_FieldFmt!=DSREND_FIELD_FORMAT_AUTO)
+		{
+			name+=",";
+		}
+		
+		if(m_VideoFmt[pos].m_FieldFmt==DSREND_FIELD_FORMAT_FRAME)
+		{
+			name+="Frame";
+		}
+		else if(m_VideoFmt[pos].m_FieldFmt==DSREND_FIELD_FORMAT_FIELD)
+		{
+			name+="Field";
+		}
+		
+		name+=")";
+	}
+
+	SetDlgItemText(IDC_DSHOW_VIDEOFMTS_NAME,name);
+}
+
+void CDSVideoFormatPage::OnDeltaPosWidth(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnDeltaPosWidth\n"),__FILE__,__LINE__);
+	NM_UPDOWN* pNMUpDown = (NM_UPDOWN*)pNMHDR;
+	
+	//spin to nearest multiple of 16
+	if(pNMUpDown->iDelta>0)
+	{
+		//spin up
+		pNMUpDown->iDelta=pNMUpDown->iDelta*16-(pNMUpDown->iPos%16);
+	}
+	else
+	{
+		//spin down
+		pNMUpDown->iDelta=pNMUpDown->iDelta*16+(pNMUpDown->iPos%16!=0 ? 16-(pNMUpDown->iPos%16) : 0);
+	}
+
+	*pResult = 0;
+}
+
+void CDSVideoFormatPage::OnDeltaPosHeight(NMHDR* pNMHDR, LRESULT* pResult) 
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnDeltaPosHeight\n"),__FILE__,__LINE__);
+	NM_UPDOWN* pNMUpDown = (NM_UPDOWN*)pNMHDR;
+	
+	//spin to nearest multiple of 2
+	if(pNMUpDown->iDelta>0)
+	{
+		//spin up
+		pNMUpDown->iDelta=pNMUpDown->iDelta*2-(pNMUpDown->iPos%2);
+	}
+	else
+	{
+		//spin down
+		pNMUpDown->iDelta=pNMUpDown->iDelta*2+(pNMUpDown->iPos%2!=0 ? 2-(pNMUpDown->iPos%2) : 0);
+	}
+
+	*pResult = 0;
+}
+
+void CDSVideoFormatPage::OnChangeWidth()
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+	
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnChangeWidth\n"),__FILE__,__LINE__);
+	if(m_hWnd!=NULL && m_ListBox.m_hWnd!=NULL)
+	{
+		int width=GetDlgItemInt(IDC_DSHOW_VIDEOFMTS_WIDTH);
+		///@todo check that the width is valid
+
+		int CurSel=m_ListBox.GetCurSel();
+		if(CurSel!=LB_ERR)
+		{
+			DWORD pos=m_ListBox.GetItemData(CurSel);
+			m_VideoFmt[pos].m_Width=width;
+			
+			GenerateName(pos);
+		}
+	}
+}
+
+void CDSVideoFormatPage::OnChangeHeight()
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+	
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnChangeHeight\n"),__FILE__,__LINE__);
+	if(m_hWnd!=NULL && m_ListBox.m_hWnd!=NULL)
+	{
+		int height=GetDlgItemInt(IDC_DSHOW_VIDEOFMTS_HEIGHT);
+		///@todo check that the height is valid
+
+		int CurSel=m_ListBox.GetCurSel();
+		if(CurSel!=LB_ERR)
+		{
+			DWORD pos=m_ListBox.GetItemData(CurSel);
+			m_VideoFmt[pos].m_Height=height;
+			
+			GenerateName(pos);
+		}
+	}
+}
+
+void CDSVideoFormatPage::OnChangeName()
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnChangeName\n"),__FILE__,__LINE__);
+	int CurSel=m_ListBox.GetCurSel();
+	if(CurSel!=LB_ERR)
+	{
+		DWORD pos=m_ListBox.GetItemData(CurSel);
+		CString name;
+		GetDlgItemText(IDC_DSHOW_VIDEOFMTS_NAME,name);
+		m_VideoFmt[pos].m_Name=name;
+		
+		//replace item in the listbox with the new name
+		m_ListBox.DeleteString(CurSel);
+		CurSel=m_ListBox.InsertString(CurSel,name);
+		m_ListBox.SetItemData(CurSel,pos);
+		m_ListBox.SetCurSel(CurSel);
+	}
+}
+
+void CDSVideoFormatPage::OnClickedYUY2()
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+
+	int CurSel=m_ListBox.GetCurSel();
+	if(CurSel!=LB_ERR)
+	{
+		DWORD pos=m_ListBox.GetItemData(CurSel);
+		m_VideoFmt[pos].m_bForceYUY2=m_YUY2Check.GetCheck()==BST_CHECKED;
+		GenerateName(pos);
+	}
+}
+
+void CDSVideoFormatPage::OnSelEndOkFieldFmt()
+{
+	if(m_bInUpdateControlls)
+	{
+		return;
+	}
+
+	int CurSel=m_ListBox.GetCurSel();
+	if(CurSel!=LB_ERR)
+	{
+		DWORD pos=m_ListBox.GetItemData(CurSel);
+		m_VideoFmt[pos].m_FieldFmt=(DSREND_FIELD_FORMAT)m_SampleFormat.GetItemData(m_SampleFormat.GetCurSel());
+		GenerateName(pos);
+	}
+}
+
+void CDSVideoFormatPage::OnContextMenu(CWnd* pWnd, CPoint point) 
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnContextMenu\n"),__FILE__,__LINE__);
+	CPoint p;
+	GetCursorPos(&p);
+	m_ListBox.ScreenToClient(&p);
+	CRect rect;
+	m_ListBox.GetClientRect(&rect);
+
+	//is the click inside the listbox?
+	if(rect.PtInRect(p)==TRUE)
+	{
+		CMenu popup;
+		popup.LoadMenu(IDC_DSHOW_VIDEOFMTS_CONTEXTMENU);
+
+		//enable/disable delete menu entry
+		BOOL bOutside;
+		UINT item=m_ListBox.ItemFromPoint(p,bOutside);
+		if(bOutside==FALSE)
+		{
+			popup.EnableMenuItem(IDC_DSHOW_VIDEOFMTS_DELETE,MF_BYCOMMAND|MF_ENABLED);
+			m_ListBox.SetCurSel(item);
+		}
+		else
+		{
+			popup.EnableMenuItem(IDC_DSHOW_VIDEOFMTS_DELETE,MF_BYCOMMAND|MF_DISABLED|MF_GRAYED);
+		}
+		SetForegroundWindow();
+		GetCursorPos(&p);
+		popup.GetSubMenu(0)->TrackPopupMenu(TPM_LEFTALIGN,p.x, p.y,this);
+	}
+}
+
+void CDSVideoFormatPage::OnClickedDelete()
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnClickedDelete\n"),__FILE__,__LINE__);
+}
+
+void CDSVideoFormatPage::OnClickedNew()
+{
+	TRACE(_T("%s(%d) : CDSVideoFormatPage::OnClickedNew\n"),__FILE__,__LINE__);
+
+	CDShowGraph::CVideoFormat NewFormat;
+	NewFormat.m_Name="<New Item>";
+	m_VideoFmt.push_back(NewFormat);
+	int pos=m_ListBox.AddString(NewFormat.m_Name.c_str());
+	m_ListBox.SetItemData(pos,m_VideoFmt.size()-1);
+}
---
+++ DScaler/dshowsource/DSVideoFormatPage.h
@@ -0,0 +1,109 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: DSVideoFormatPage.h,v 1.1 2002-09-04 17:08:31 tobbej Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Torbjörn Jansson.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+//
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * @file DSVideoFormatPage.h header file
+ */
+
+#if !defined(AFX_DSVIDEOFORMATPAGE_H__24867333_526D_4B36_8A75_E64A92C140A3__INCLUDED_)
+#define AFX_DSVIDEOFORMATPAGE_H__24867333_526D_4B36_8A75_E64A92C140A3__INCLUDED_
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+#include "TreeSettingsPage.h"
+#include "..\DScalerRes\resource.h"
+#include "DSGraph.h"
+#include <vector>
+#include "HSListBox.h"
+
+/**
+ * CDSVideoFormatPage dialog
+ */
+class CDSVideoFormatPage : public CTreeSettingsPage
+{
+// Construction
+public:
+	CDSVideoFormatPage(CString name,vector<CDShowGraph::CVideoFormat> &fmts,CSimpleSetting *pResolution);   // standard constructor
+
+// Dialog Data
+	//{{AFX_DATA(CDSVideoFormatPage)
+	enum { IDD = IDD_DSHOW_VIDEOFMTS };
+	CListBox m_ListBox;
+	CSpinButtonCtrl	m_SpinWidth;
+	CSpinButtonCtrl	m_SpinHeight;
+	CButton m_YUY2Check;
+	CComboBox m_SampleFormat;
+	//}}AFX_DATA
+
+
+// Overrides
+	// ClassWizard generated virtual function overrides
+	//{{AFX_VIRTUAL(CDSVideoFormatPage)
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+	//}}AFX_VIRTUAL
+
+// Implementation
+protected:
+
+	// Generated message map functions
+	//{{AFX_MSG(CDSVideoFormatPage)
+	virtual BOOL OnInitDialog();
+	afx_msg void OnVideoFmtSelChange();
+	afx_msg void OnDeltaPosWidth(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnDeltaPosHeight(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnChangeWidth();
+	afx_msg void OnChangeHeight();
+	afx_msg void OnChangeName();
+	afx_msg void OnClickedYUY2();
+	afx_msg void OnSelEndOkFieldFmt();
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	afx_msg void OnClickedDelete();
+	afx_msg void OnClickedNew();
+	//}}AFX_MSG
+	DECLARE_MESSAGE_MAP()
+	
+	void OnOK();
+
+private:
+	void UpdateControlls();
+	void GenerateName(int pos);
+	vector<CDShowGraph::CVideoFormat> m_VideoFmt;
+	vector<CDShowGraph::CVideoFormat> &m_RealVideoFmt;
+
+	///flag to prevent controlls from messing with the settings when updating controlls
+	bool m_bInUpdateControlls;
+	CSimpleSetting *m_pResolutionSetting;
+};
+
+//{{AFX_INSERT_LOCATION}}
+// Microsoft Visual C++ will insert additional declarations immediately before the previous line.
+
+#endif // !defined(AFX_DSVIDEOFORMATPAGE_H__24867333_526D_4B36_8A75_E64A92C140A3__INCLUDED_)

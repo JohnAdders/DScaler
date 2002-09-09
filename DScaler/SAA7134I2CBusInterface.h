673c67c6a8c4de29999de193849a9ce76756aad0
atnak <atnak@users.sourceforge.net>
2002-09-09 14:25:16 +0000

    forgot to add header comments, fixed
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2171 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/SAA7134I2CBusInterface.h
+++ DScaler/SAA7134I2CBusInterface.h
@@ -1,3 +1,37 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: SAA7134I2CBusInterface.h,v 1.2 2002-09-09 14:25:16 atnak Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Atsushi Nakagawa.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+//
+// This software was based on I2CBus.cpp.  Those portions are
+// copyleft 2001 itt@myself.com.
+//
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 09 Sep 2002   Atsushi Nakagawa      Initial Release
+//
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+//
+//////////////////////////////////////////////////////////////////////////////
 
 #ifndef __SAA7134I2CBUSINTERFACE_H__
 #define __SAA7134I2CBUSINTERFACE_H__
--- DScaler/SAA7134I2CInterface.h
+++ DScaler/SAA7134I2CInterface.h
@@ -1,3 +1,33 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: SAA7134I2CInterface.h,v 1.2 2002-09-09 14:25:16 atnak Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2002 Atsushi Nakagawa.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 09 Sep 2002   Atsushi Nakagawa      Initial Release
+//
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+//
+//////////////////////////////////////////////////////////////////////////////
+
 #ifndef __SAA7134I2CINTERFACE_H__
 #define __SAA7134I2CINTERFACE_H__
 

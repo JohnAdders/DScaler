db199a84dabfdac4113c4807085d3dc4b34a65b3
ittarnavsky <ittarnavsky@users.sourceforge.net>
2002-09-12 21:50:59 +0000

    Added for the change from IAudioControls to CAudioControls
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2190 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ DScaler/AudioControls.cpp
@@ -0,0 +1,178 @@
+//
+// $Id: AudioControls.cpp,v 1.1 2002-09-12 21:50:59 ittarnavsky Exp $
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// copyleft 2002 itt@myself.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+#include "stdafx.h"
+#include "AudioControls.h"
+
+
+CAudioControls::CAudioControls()
+{
+}
+
+CAudioControls::~CAudioControls()
+{
+}
+
+
+bool CAudioControls::HasMute()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetMute(bool bMute)
+{
+}
+
+bool CAudioControls::IsMuted()
+{
+    return FALSE;
+}
+
+bool CAudioControls::HasVolume()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetVolume(WORD nVolume)
+{
+}
+
+WORD CAudioControls::GetVolume()
+{
+    return 0;
+}
+
+bool CAudioControls::HasBalance()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetBalance(WORD nBalance)
+{
+}
+
+WORD CAudioControls::GetBalance()
+{
+    return 0;
+}
+
+bool CAudioControls::HasBass()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetBass(WORD nLevel)
+{
+}
+
+WORD CAudioControls::GetBass()
+{
+    return 0;
+}
+
+bool CAudioControls::HasTreble()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetTreble(WORD nLevel)
+{
+}
+
+WORD CAudioControls::GetTreble()
+{
+    return 0;
+}
+
+bool CAudioControls::HasEqualizers()
+{
+    return FALSE;
+}
+
+WORD CAudioControls::GetEqualizerCount()
+{
+    return 0;
+}
+
+void CAudioControls::SetEqualizerLevel(WORD nIndex, WORD nLevel)
+{
+}
+
+WORD CAudioControls::GetEqualizerLevel(WORD nIndex)
+{
+    return 0;
+}
+
+bool CAudioControls::HasBassBoost()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetBassBoost(bool bBoost)
+{
+}
+
+bool CAudioControls::IsBassBoosted()
+{
+    return FALSE;
+}
+
+bool CAudioControls::HasLoudness()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetLoudness(WORD nLevel)
+{
+}
+
+WORD CAudioControls::GetLoudness()
+{
+    return 0;
+}
+
+bool CAudioControls::HasSpatialEffect()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetSpatialEffect(WORD nLevel)
+{
+}
+
+WORD CAudioControls::GetSpatialEffect()
+{
+    return 0;
+}
+
+bool CAudioControls::HasDolby()
+{
+    return FALSE;
+}
+
+void CAudioControls::SetDolby(WORD nMode, WORD nNoise, WORD nSpatial, WORD nPan, WORD nPanorama)
+{
+}
---
+++ DScaler/AudioControls.h
@@ -0,0 +1,74 @@
+//
+// $Id: AudioControls.h,v 1.1 2002-09-12 21:50:59 ittarnavsky Exp $
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// copyleft 2002 itt@myself.com
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+#if !defined(__AUDIOCONTROLS_H__)
+#define __AUDIOCONTROLS_H__
+
+#if _MSC_VER > 1000
+#pragma once
+#endif // _MSC_VER > 1000
+
+// \todo: create a generic audio controls enumeration
+
+class CAudioControls
+{
+public:
+    CAudioControls();
+    virtual ~CAudioControls();
+    virtual bool HasMute();
+    virtual void SetMute(bool bMute = true);
+    virtual bool IsMuted();
+    virtual bool HasVolume();
+    virtual void SetVolume(WORD nVolume);
+    virtual WORD GetVolume();
+    virtual bool HasBalance();
+    virtual void SetBalance(WORD nBalance);
+    virtual WORD GetBalance();
+    virtual bool HasBass();
+    virtual void SetBass(WORD nLevel);
+    virtual WORD GetBass();
+    virtual bool HasTreble();
+    virtual void SetTreble(WORD nLevel);
+    virtual WORD GetTreble();
+    virtual bool HasEqualizers();
+    virtual WORD GetEqualizerCount();
+    virtual void SetEqualizerLevel(WORD nIndex, WORD nLevel);
+    virtual WORD GetEqualizerLevel(WORD nIndex);
+    virtual bool HasBassBoost();
+    virtual void SetBassBoost(bool bBoost);
+    virtual bool IsBassBoosted();
+    virtual bool HasLoudness();
+    virtual void SetLoudness(WORD nLevel);
+    virtual WORD GetLoudness();
+    virtual bool HasSpatialEffect();
+    virtual void SetSpatialEffect(WORD nLevel);
+    virtual WORD GetSpatialEffect();
+    virtual bool HasDolby();
+    virtual void SetDolby(WORD nMode, WORD nNoise, WORD nSpatial, WORD nPan, WORD nPanorama);
+    // \todo: define a getter for Dolby
+};
+
+#endif // !defined(__AUDIOCONTROLS_H__)

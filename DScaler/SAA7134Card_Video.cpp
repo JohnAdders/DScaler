1ea167bc7f8da32fcdde6d6c703a1d7d42573d6d
atnak <atnak@users.sourceforge.net>
2002-10-06 11:11:29 +0000

    Added SetVSync recovery
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2333 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/SAA7134Card_Video.cpp
+++ DScaler/SAA7134Card_Video.cpp
@@ -1,36 +1,39 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: SAA7134Card_Video.cpp,v 1.1 2002-10-03 23:42:07 atnak Exp $
+// $Id: SAA7134Card_Video.cpp,v 1.2 2002-10-06 11:11:29 atnak Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2002 Atsushi Nakagawa.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
 //
-//	This file is subject to the terms of the GNU General Public License as
-//	published by the Free Software Foundation.	A copy of this license is
-//	included with this software distribution in the file COPYING.  If you
-//	do not have a copy, you may obtain a copy by writing to the Free
-//	Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
 //
-//	This software is distributed in the hope that it will be useful,
-//	but WITHOUT ANY WARRANTY; without even the implied warranty of
-//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//	GNU General Public License for more details
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
 /////////////////////////////////////////////////////////////////////////////
 //
 // This software was based on v4l2 device driver for philips
-// saa7134 based TV cards.	Those portions are
+// saa7134 based TV cards.  Those portions are
 // Copyright (c) 2001,02 Gerd Knorr <kraxel@bytesex.org> [SuSE Labs]
 //
 /////////////////////////////////////////////////////////////////////////////
 // Change Log
 //
-// Date 		 Developer			   Changes
+// Date          Developer             Changes
 //
-// 03 Oct 2002	 Atsushi Nakagawa	   Moved video setting stuff in here
+// 03 Oct 2002   Atsushi Nakagawa      Moved video setting stuff in here
 //
 /////////////////////////////////////////////////////////////////////////////
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2002/10/03 23:42:07  atnak
+// SAA7134Common.h SAA7134Common.cpp SAA7134Card_Video.cpp added
+//
 //
 //////////////////////////////////////////////////////////////////////////////
 
@@ -42,42 +45,42 @@
 
 void CSAA7134Card::SetBrightness(BYTE Brightness)
 {
-	WriteByte(SAA7134_DEC_LUMA_BRIGHT, Brightness);
+    WriteByte(SAA7134_DEC_LUMA_BRIGHT, Brightness);
 }
 
 BYTE CSAA7134Card::GetBrightness()
 {
-	return ReadByte(SAA7134_DEC_LUMA_BRIGHT);
+    return ReadByte(SAA7134_DEC_LUMA_BRIGHT);
 }
 
 void CSAA7134Card::SetContrast(BYTE Contrast)
 {
-	WriteByte(SAA7134_DEC_LUMA_CONTRAST, Contrast);
+    WriteByte(SAA7134_DEC_LUMA_CONTRAST, Contrast);
 }
 
 BYTE CSAA7134Card::GetContrast()
 {
-	return ReadByte(SAA7134_DEC_LUMA_CONTRAST);
+    return ReadByte(SAA7134_DEC_LUMA_CONTRAST);
 }
 
 void CSAA7134Card::SetSaturation(BYTE SaturationU)
 {
-	WriteByte(SAA7134_DEC_CHROMA_SATURATION, SaturationU);
+    WriteByte(SAA7134_DEC_CHROMA_SATURATION, SaturationU);
 }
 
 BYTE CSAA7134Card::GetSaturation()
 {
-	return ReadByte(SAA7134_DEC_CHROMA_SATURATION);
+    return ReadByte(SAA7134_DEC_CHROMA_SATURATION);
 }
 
 void CSAA7134Card::SetHue(BYTE Hue)
 {
-	WriteByte(SAA7134_DEC_CHROMA_HUE, Hue);
+    WriteByte(SAA7134_DEC_CHROMA_HUE, Hue);
 }
 
 BYTE CSAA7134Card::GetHue()
 {
-	return ReadByte(SAA7134_DEC_CHROMA_HUE);
+    return ReadByte(SAA7134_DEC_CHROMA_HUE);
 }
 
 // Find SAA7134 equivalent or remove
@@ -88,19 +91,19 @@ void CSAA7134Card::SetWhiteCrushUp(BYTE WhiteCrushUp)
 // Find SAA7134 equivalent or remove
 BYTE CSAA7134Card::GetWhiteCrushUp()
 {
-	return 0x00;
+    return 0x00;
 }
 
 // Find SAA7134 equivalent or remove
 void CSAA7134Card::SetWhiteCrushDown(BYTE WhiteCrushDown)
 {
-//	  WriteByte(BT848_WC_DOWN, WhiteCrushDown);
+//    WriteByte(BT848_WC_DOWN, WhiteCrushDown);
 }
 
 // Find SAA7134 equivalent or remove
 BYTE CSAA7134Card::GetWhiteCrushDown()
 {
-	return 0x00;
+    return 0x00;
 }
 
 // Find SAA7134 equivalent or remove
@@ -111,7 +114,7 @@ void CSAA7134Card::SetBDelay(BYTE BDelay)
 // Find SAA7134 equivalent or remove
 BYTE CSAA7134Card::GetBDelay()
 {
-	return 0x00;
+    return 0x00;
 }
 
 
@@ -119,11 +122,11 @@ void CSAA7134Card::SetVideoStandard(eVideoStandard VideoStandard, long& VBILines
                                     long& VideoWidth, long& VideoHeight,
                                     long HDelayShift, long VDelayShift)
 {
-	BYTE SyncControl    = 0;
-	BYTE LumaControl    = 0;
-	BYTE ChromaCtrl1    = 0;
-	BYTE ChromaGain     = 0;
-	BYTE ChromaCtrl2    = 0;
+    BYTE SyncControl    = 0;
+    BYTE LumaControl    = 0;
+    BYTE ChromaCtrl1    = 0;
+    BYTE ChromaGain     = 0;
+    BYTE ChromaCtrl2    = 0;
     BYTE MiscVGateMSB   = 0;
 
     if (VideoStandard == VIDEOSTANDARD_AUTODETECT)
@@ -201,20 +204,20 @@ void CSAA7134Card::SetVideoStandard(eVideoStandard VideoStandard, long& VBILines
 
     UpdateAudioClocksPerField(VideoStandard);
 
-	WORD VBIStartLine   = m_VideoStandards[VideoStandard].wVBIStartLine;
-	WORD VBIStopLine    = m_VideoStandards[VideoStandard].wVBIStopLine;
+    WORD VBIStartLine   = m_VideoStandards[VideoStandard].wVBIStartLine;
+    WORD VBIStopLine    = m_VideoStandards[VideoStandard].wVBIStopLine;
 
-	if (VBIStopLine - VBIStartLine + 1 > VBILines)
-	{
-		VBIStopLine = VBIStartLine + VBILines - 1;
-	}
-	else
-	{
-		VBILines = VBIStopLine - VBIStartLine + 1;
-	}
+    if (VBIStopLine - VBIStartLine + 1 > VBILines)
+    {
+        VBIStopLine = VBIStartLine + VBILines - 1;
+    }
+    else
+    {
+        VBILines = VBIStopLine - VBIStartLine + 1;
+    }
 
-	SetVBIGeometry(TASKID_A, 0, 719, VBIStartLine, VBIStopLine);
-	SetVBIGeometry(TASKID_B, 0, 719, VBIStartLine, VBIStopLine);
+    SetVBIGeometry(TASKID_A, 0, 719, VBIStartLine, VBIStopLine);
+    SetVBIGeometry(TASKID_B, 0, 719, VBIStartLine, VBIStopLine);
 
     VideoHeight = m_VideoStandards[VideoStandard].wFieldHeight * 2;
 
@@ -230,8 +233,8 @@ void CSAA7134Card::SetGeometry(WORD ScaleWidth, WORD ScaleHeight,
         return;
     }
 
-	WORD FieldWidth     = m_VideoStandards[m_VideoStandard].wFieldWidth;
-	WORD FieldHeight    = m_VideoStandards[m_VideoStandard].wFieldHeight;
+    WORD FieldWidth     = m_VideoStandards[m_VideoStandard].wFieldWidth;
+    WORD FieldHeight    = m_VideoStandards[m_VideoStandard].wFieldHeight;
 
     WORD HDelay         = m_VideoStandards[m_VideoStandard].wHDelay;
     WORD VDelay         = m_VideoStandards[m_VideoStandard].wVDelay;
@@ -272,41 +275,41 @@ void CSAA7134Card::SetGeometry(WORD ScaleWidth, WORD ScaleHeight,
 void CSAA7134Card::SetVBIGeometry(eTaskID TaskID, WORD HStart, WORD HStop,
                                   WORD VStart, WORD VStop)
 {
-	BYTE TaskMask = TaskID2TaskMask(TaskID);
+    BYTE TaskMask = TaskID2TaskMask(TaskID);
 
-	// It should come out less than this amount
-	WORD SampleBytes = 1024;
+    // It should come out less than this amount
+    WORD SampleBytes = 1024;
 
-	WriteWord(SAA7134_VBI_H_START(TaskMask), HStart);
-	WriteWord(SAA7134_VBI_H_STOP(TaskMask), HStop);
-	WriteWord(SAA7134_VBI_V_START(TaskMask), VStart);
-	WriteWord(SAA7134_VBI_V_STOP(TaskMask), VStop);
+    WriteWord(SAA7134_VBI_H_START(TaskMask), HStart);
+    WriteWord(SAA7134_VBI_H_STOP(TaskMask), HStop);
+    WriteWord(SAA7134_VBI_V_START(TaskMask), VStart);
+    WriteWord(SAA7134_VBI_V_STOP(TaskMask), VStop);
 
-	// SAA7134_VBI_H_SCALE_INC:
-	//	 0x400 for 100%, 0x200 for 200%
+    // SAA7134_VBI_H_SCALE_INC:
+    //   0x400 for 100%, 0x200 for 200%
 
-	// (This may be specific to PAL-BG)
-	// DScaler wants exactly 0x0186 horizontal scaling but SAA7134
-	// can't handle this scaling.  Instead, we scale 0x30C (half of
-	// 0x0186) and double the bytes in SAA7134Source.cpp
-	// WriteWord(SAA7134_VBI_H_SCALE_INC(TaskMask), 0x0186);
-	//
-	WriteWord(SAA7134_VBI_H_SCALE_INC(TaskMask), 0x030C);
+    // (This may be specific to PAL-BG)
+    // DScaler wants exactly 0x0186 horizontal scaling but SAA7134
+    // can't handle this scaling.  Instead, we scale 0x30C (half of
+    // 0x0186) and double the bytes in SAA7134Source.cpp
+    // WriteWord(SAA7134_VBI_H_SCALE_INC(TaskMask), 0x0186);
+    //
+    WriteWord(SAA7134_VBI_H_SCALE_INC(TaskMask), 0x030C);
 
-	WORD Lines = VStop - VStart + 1;
+    WORD Lines = VStop - VStart + 1;
 
-	eRegionID RegionID = TaskID2VBIRegion(TaskID);
-	if (GetDMA(RegionID))
-	{
-		// Make sure we aren't grabbing more lines than we have memory
-		WORD LinesAvailable = CalculateLinesAvailable(RegionID, SampleBytes);
+    eRegionID RegionID = TaskID2VBIRegion(TaskID);
+    if (GetDMA(RegionID))
+    {
+        // Make sure we aren't grabbing more lines than we have memory
+        WORD LinesAvailable = CalculateLinesAvailable(RegionID, SampleBytes);
 
-		if (LinesAvailable < Lines)
-			Lines = LinesAvailable;
-	}
+        if (LinesAvailable < Lines)
+            Lines = LinesAvailable;
+    }
 
-	WriteWord(SAA7134_VBI_H_LEN(TaskMask), SampleBytes);
-	WriteWord(SAA7134_VBI_V_LEN(TaskMask), Lines);
+    WriteWord(SAA7134_VBI_H_LEN(TaskMask), SampleBytes);
+    WriteWord(SAA7134_VBI_V_LEN(TaskMask), Lines);
 }
 
 
@@ -315,22 +318,22 @@ void CSAA7134Card::SetTaskGeometry(eTaskID TaskID,
                                    WORD HDelay, WORD VDelay,
                                    WORD ScaleWidth, WORD ScaleHeight)
 {
-	BYTE TaskMask = TaskID2TaskMask(TaskID);
+    BYTE TaskMask = TaskID2TaskMask(TaskID);
 
-	WriteWord(SAA7134_VIDEO_H_START(TaskMask), HDelay);
-	WriteWord(SAA7134_VIDEO_H_STOP(TaskMask), HDelay + Width - 1);
-	WriteWord(SAA7134_VIDEO_V_START(TaskMask), VDelay);
-	WriteWord(SAA7134_VIDEO_V_STOP(TaskMask), VDelay + Height - 1);
+    WriteWord(SAA7134_VIDEO_H_START(TaskMask), HDelay);
+    WriteWord(SAA7134_VIDEO_H_STOP(TaskMask), HDelay + Width - 1);
+    WriteWord(SAA7134_VIDEO_V_START(TaskMask), VDelay);
+    WriteWord(SAA7134_VIDEO_V_STOP(TaskMask), VDelay + Height - 1);
 
-	// settings for no horizontal prescaling
-	ResetHPrescale(TaskID);
+    // settings for no horizontal prescaling
+    ResetHPrescale(TaskID);
 
-	// 1024UL = median
-	WORD HorizScale = 1024UL * Width / ScaleWidth;
-	WriteWord(SAA7134_H_SCALE_INC(TaskMask), HorizScale);
+    // 1024UL = median
+    WORD HorizScale = 1024UL * Width / ScaleWidth;
+    WriteWord(SAA7134_H_SCALE_INC(TaskMask), HorizScale);
 
-	// settings for vertical scaling
-	WORD VertScale = 1024UL * Height / ScaleHeight;
+    // settings for vertical scaling
+    WORD VertScale = 1024UL * Height / ScaleHeight;
     WriteWord(SAA7134_V_SCALE_RATIO(TaskMask), VertScale);
 
     WORD Lines = ScaleHeight;
@@ -354,7 +357,7 @@ void CSAA7134Card::SetTaskGeometry(eTaskID TaskID,
 
 void CSAA7134Card::CheckVBIAndVideoOverlap(eTaskID TaskID)
 {
-	BYTE TaskMask = TaskID2TaskMask(TaskID);
+    BYTE TaskMask = TaskID2TaskMask(TaskID);
 
     WORD VBIStopLine = ReadWord(SAA7134_VBI_V_STOP(TaskMask));
     WORD VIDStartLine = ReadWord(SAA7134_VIDEO_V_START(TaskMask));
@@ -409,6 +412,36 @@ void CSAA7134Card::SetHPLLMode(eHPLLMode HPLLMode)
     {
         MaskDataByte(SAA7134_SYNC_CTRL, 0x18, SAA7134_SYNC_CTRL_HTD);
     }
+}
+
+
+void CSAA7134Card::SetVSyncRecovery(eVSyncRecovery VSyncRecovery)
+{
+    BYTE VNOI = 0x00;
+
+    switch (VSyncRecovery)
+    {
+    case VSYNCRECOVERY_NORMAL:
+        VNOI = 0x00;
+        break;
+    case VSYNCRECOVERY_FAST_TRACKING:
+        if (ReadByte(SAA7134_SYNC_CTRL) & SAA7134_SYNC_CTRL_AUFD)
+        {
+            VNOI = 0x00;
+        }
+        else
+        {
+            VNOI = 0x01;
+        }
+        break;
+    case VSYNCRECOVERY_FREE_RUNNING:
+        VNOI = 0x02;
+        break;
+    case VSYNCRECOVERY_IMMEDIATE_MODE:
+        VNOI = 0x03;
+        break;
+    }
+    MaskDataByte(SAA7134_SYNC_CTRL, VNOI, SAA7134_SYNC_CTRL_VNOI);
 }
 
 
@@ -467,35 +500,35 @@ BOOL CSAA7134Card::IsInterlacedSignalDetected()
 // - This is "prescaling" as opposed to scaling.. don't know what that means
 void CSAA7134Card::SetHPrescale(eTaskID TaskID, WORD wSourceSize, WORD wScaleSize)
 {
-	BYTE TaskMask = TaskID2TaskMask(TaskID);
+    BYTE TaskMask = TaskID2TaskMask(TaskID);
 
-	static const struct {
-		int xpsc;
-		int xacl;
-		int xc2_1;
-		int xdcg;
-		int vpfy;
-	} vals[] = {
-		/* XPSC XACL XC2_1 XDCG VPFY */
-		{	 1,   0,	0,	  0,   0 },
-		{	 2,   2,	1,	  2,   2 },
-		{	 3,   4,	1,	  3,   2 },
-		{	 4,   8,	1,	  4,   2 },
-		{	 5,   8,	1,	  4,   2 },
-		{	 6,   8,	1,	  4,   3 },
-		{	 7,   8,	1,	  4,   3 },
-		{	 8,  15,	0,	  4,   3 },
-		{	 9,  15,	0,	  4,   3 },
-		{	10,  16,	1,	  5,   3 },
-	};
+    static const struct {
+        int xpsc;
+        int xacl;
+        int xc2_1;
+        int xdcg;
+        int vpfy;
+    } vals[] = {
+        /* XPSC XACL XC2_1 XDCG VPFY */
+        {    1,   0,    0,    0,   0 },
+        {    2,   2,    1,    2,   2 },
+        {    3,   4,    1,    3,   2 },
+        {    4,   8,    1,    4,   2 },
+        {    5,   8,    1,    4,   2 },
+        {    6,   8,    1,    4,   3 },
+        {    7,   8,    1,    4,   3 },
+        {    8,  15,    0,    4,   3 },
+        {    9,  15,    0,    4,   3 },
+        {   10,  16,    1,    5,   3 },
+    };
 
-	int i = 2;
+    int i = 2;
 
-	WriteByte(SAA7134_H_PRESCALE(TaskMask), vals[i].xpsc);
-	WriteByte(SAA7134_ACC_LENGTH(TaskMask), vals[i].xacl);
-	WriteByte(SAA7134_LEVEL_CTRL(TaskMask), (vals[i].xc2_1 << 3) | (vals[i].xdcg));
-	MaskDataByte(SAA7134_FIR_PREFILTER_CTRL(TaskMask),
-		(vals[i].vpfy << 2) | vals[i].vpfy, 0x0F);
+    WriteByte(SAA7134_H_PRESCALE(TaskMask), vals[i].xpsc);
+    WriteByte(SAA7134_ACC_LENGTH(TaskMask), vals[i].xacl);
+    WriteByte(SAA7134_LEVEL_CTRL(TaskMask), (vals[i].xc2_1 << 3) | (vals[i].xdcg));
+    MaskDataByte(SAA7134_FIR_PREFILTER_CTRL(TaskMask),
+        (vals[i].vpfy << 2) | vals[i].vpfy, 0x0F);
 }
 
 

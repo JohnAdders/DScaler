c439661d04188720bcd1052810c49c86248a9568
atnak <atnak@users.sourceforge.net>
2003-01-01 20:42:13 +0000

    Enclosed the functions into a class
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2816 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/VTMosaicGraphics.cpp
+++ DScaler/VTMosaicGraphics.cpp
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: VTMosaicGraphics.cpp,v 1.1 2002-01-15 11:16:03 temperton Exp $
+// $Id: VTMosaicGraphics.cpp,v 1.2 2003-01-01 20:42:13 atnak Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2001 Mike Temperton.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -15,57 +15,85 @@
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Library General Public License for more details
 /////////////////////////////////////////////////////////////////////////////
+//
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 03 Jan 2003   Atsushi Nakagawa      Enclosed the functions into a class.
+//
+/////////////////////////////////////////////////////////////////////////////
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2002/01/15 11:16:03  temperton
+// New teletext drawing code.
+//
 //
 /////////////////////////////////////////////////////////////////////////////
+
 
 #include "stdafx.h"
 #include "VTMosaicGraphics.h"
 
 
-BYTE graf[64] =
-  { 0x00, 0x20, 0x10, 0x30, 0x08, 0x28, 0x18, 0x38, 0x04, 0x24, 0x14, 0x34, 0x0C, 0x2C, 0x1C, 0x3C,
+BYTE CVTMosaicGraphics::m_G1MosaicSet[64] =
+{
+    0x00, 0x20, 0x10, 0x30, 0x08, 0x28, 0x18, 0x38, 0x04, 0x24, 0x14, 0x34, 0x0C, 0x2C, 0x1C, 0x3C,
     0x02, 0x22, 0x12, 0x32, 0x0A, 0x2A, 0x1A, 0x3A, 0x06, 0x26, 0x16, 0x36, 0x0E, 0x2E, 0x1E, 0x3E,
     0x01, 0x21, 0x11, 0x31, 0x09, 0x29, 0x19, 0x39, 0x05, 0x25, 0x15, 0x35, 0x0D, 0x2D, 0x1D, 0x3D,
-    0x03, 0x23, 0x13, 0x33, 0x0B, 0x2B, 0x1B, 0x3B, 0x07, 0x27, 0x17, 0x37, 0x0F, 0x2F, 0x1F, 0x3F };
-
-BYTE xmgraf[2][2] =
-   { 0, 0,
-     0, 1};
-
-BYTE ymgraf[3][3] =
-   { 0, 0, 0,
-     0, 0, 1,
-     0, 1, 1 };
-
-BYTE btable[3][2] =
-   { 32, 16,
-     8, 4,
-     2, 1 };
+    0x03, 0x23, 0x13, 0x33, 0x0B, 0x2B, 0x1B, 0x3B, 0x07, 0x27, 0x17, 0x37, 0x0F, 0x2F, 0x1F, 0x3F,
+};
 
 
-void DrawGraphChar(HDC hDC, HBRUSH hBrush, int Char, BOOL Separated, int left, int top, int width, int height)
+BYTE CVTMosaicGraphics::m_xmgraf[2][2] =
 {
-    int w, h;
-    int x_m = width % 2;
-    int y_m = height % 3;
-    int x_d = width / 2;
-    int y_d = height / 3;
+    0, 0,
+    0, 1,
+};
 
-    for(int x = 0; x < 2; x++)
+
+BYTE CVTMosaicGraphics::m_ymgraf[3][3] =
+{
+    0, 0, 0,
+    0, 0, 1,
+    0, 1, 1,
+};
+
+
+BYTE CVTMosaicGraphics::m_BitTable[3][2] =
+{
+    0x20, 0x10,
+    0x08, 0x04,
+    0x02, 0x01,
+};
+
+
+void CVTMosaicGraphics::DrawG1Mosaic(HDC hDC, LPRECT lpRect, BYTE uChar,
+                                      HBRUSH hBrush, bool bSeparated)
+{
+    int x_m = (lpRect->right - lpRect->left) % 2;
+    int y_m = (lpRect->bottom - lpRect->top) % 3;
+    int x_d = (lpRect->right - lpRect->left) / 2;
+    int y_d = (lpRect->bottom - lpRect->top) / 3;
+    int w, h;
+
+    uChar = (uChar & 0x1f) | ((uChar & 0x40) >> 1);
+
+    for (int x = 0; x < 2; x++)
     {
-        for(int y = 0; y < 3; y++)
+        for (int y = 0; y < 3; y++)
         {
-            if(graf[Char] & btable[y][x])
+            if (m_G1MosaicSet[uChar] & m_BitTable[y][x])
             {
                 RECT rect;
-                rect.left = left + (x ? (x_d * x + xmgraf[x_m][x-1]) : 0);
-                rect.top = top + (y ? (y_d * y + ymgraf[y_m][y-1]) : 0);
-                w = x_d + xmgraf[x_m][x];
-                h = y_d + ymgraf[y_m][y];
-                if(Separated)
+                rect.left = lpRect->left + (x ? (x_d * x + m_xmgraf[x_m][x-1]) : 0);
+                rect.top = lpRect->top + (y ? (y_d * y + m_ymgraf[y_m][y-1]) : 0);
+
+                w = x_d + m_xmgraf[x_m][x];
+                h = y_d + m_ymgraf[y_m][y];
+
+                if (bSeparated)
                 {
                     w = w * 3 / 4;
                     h = h * 3 / 4;
--- DScaler/VTMosaicGraphics.h
+++ DScaler/VTMosaicGraphics.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: VTMosaicGraphics.h,v 1.1 2002-01-15 11:16:03 temperton Exp $
+// $Id: VTMosaicGraphics.h,v 1.2 2003-01-01 20:42:12 atnak Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2001 Mike Temperton.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -15,17 +15,44 @@
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU Library General Public License for more details
 /////////////////////////////////////////////////////////////////////////////
+//
+// Change Log
+//
+// Date          Developer             Changes
+//
+// 03 Jan 2003   Atsushi Nakagawa      Enclosed the functions into a class.
+//
+/////////////////////////////////////////////////////////////////////////////
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2002/01/15 11:16:03  temperton
+// New teletext drawing code.
+//
 //
 /////////////////////////////////////////////////////////////////////////////
 
-#ifndef __VTMosaicGraphicsH__
-#define __VTMosaicGraphicsH__
+
+#ifndef __VTMOSAICGRAPHICS_H___
+#define __VTMOSAICGRAPHICS_H___
 
 #include "stdafx.h"
 
-void DrawGraphChar(HDC hDC, HBRUSH hBrush, int Char, BOOL Separated, int left, int top, int width, int height);
-     
+
+class CVTMosaicGraphics
+{
+public:
+    static void DrawG1Mosaic(HDC hDC, LPRECT lpRect, BYTE uChar,
+                             HBRUSH hBrush, bool bSeparated);
+
+private:
+    static BYTE m_G1MosaicSet[64];
+
+    static BYTE m_BitTable[3][2];
+
+    static BYTE m_xmgraf[2][2];
+    static BYTE m_ymgraf[3][3];
+};
+  
+   
 #endif

e1ca0bcd4669a8e610bb4954065569770e54d8aa
adcockj <adcockj@users.sourceforge.net>
2003-01-21 10:41:40 +0000

    Added header
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@2959 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/SettingsMaster.cpp
+++ DScaler/SettingsMaster.cpp
@@ -1,3 +1,24 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: SettingsMaster.cpp,v 1.8 2003-01-21 10:41:40 adcockj Exp $
+/////////////////////////////////////////////////////////////////////////////
+// Copyright (c) 2003 John Adcock.  All rights reserved.
+/////////////////////////////////////////////////////////////////////////////
+//
+//  This file is subject to the terms of the GNU General Public License as
+//  published by the Free Software Foundation.  A copy of this license is
+//  included with this software distribution in the file COPYING.  If you
+//  do not have a copy, you may obtain a copy by writing to the Free
+//  Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+//  This software is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details
+/////////////////////////////////////////////////////////////////////////////
+// CVS Log
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
 #include "stdafx.h"
 
 #include "Source.h"

b3ed64b82c94149bfcfa9b4af055c53caf8b4437
adcockj <adcockj@users.sourceforge.net>
2003-02-11 15:41:05 +0000

    New implementation of StrangeBob based off paper Tidy up sources
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3057 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Plugins/DI_MoComp2/MoComp2.h
+++ Plugins/DI_MoComp2/MoComp2.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: MoComp2.h,v 1.1 2003-01-02 13:15:01 adcockj Exp $
+// $Id: MoComp2.h,v 1.2 2003-02-11 15:40:50 adcockj Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 Tom Barry & John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -25,6 +25,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2003/01/02 13:15:01  adcockj
+// Added new plug-ins ready for developement by copying TomsMoComp and Gamma
+//
 /////////////////////////////////////////////////////////////////////////////
 
 #include <malloc.h>
@@ -43,85 +46,17 @@ extern long SearchEffort;
 
 static int  Fieldcopy(void *dest, const void *src, size_t count, 
 			int rows, int dst_pitch, int src_pitch);
-static int  Search_Effort_0();
-static int  Search_Effort_1();
-static int  Search_Effort_3();
-static int  Search_Effort_5();
-static int  Search_Effort_9();
-static int  Search_Effort_11();
-static int  Search_Effort_13();
-static int  Search_Effort_15();
-static int  Search_Effort_19();
-static int  Search_Effort_21();
-static int  Search_Effort_Max();
+static void  DoMoComp2_SimpleWeave();
 
-__declspec(align(128))
-static __int64 BobDiffW[2]; 
+static __int64 Save1; 
+static __int64 Save2; 
 
-__declspec(align(16))
-static __int64 BobVal[2]; 
+static __int64 MOVE  = 0x0806080608060806; 
 
-__declspec(align(16))
-static __int64 Min_Vals[2]; 
-
-__declspec(align(16))
-static __int64 Max_Vals[2]; 
-
-__declspec(align(16))
-static const __int64 Max_Mov[2] =  {0x0404040404040404,0x0404040404040404}; 
-//static const __int64 Max_Mov[2] =  {0x0f0f0f0f0f0f0f0f,0x0f0f0f0f0f0f0f0f}; 
-//static const __int64 Max_Mov[2] =  {0x0808080808080808,0x0808080808080808}; 
-
-__declspec(align(16))
-static const __int64 DiffThres[2] =  {0x0f0f0f0f0f0f0f0f,0x0f0f0f0f0f0f0f0f}; 
-
-__declspec(align(16))
-static const __int64 YMask[2] = {0x00ff00ff00ff00ff,0x00ff00ff00ff00ff}; // keeps only luma
-
-__declspec(align(16))
-static const __int64 UVMask[2] =   {0xff00ff00ff00ff00,0xff00ff00ff00ff00}; // keeps only chroma
-
-__declspec(align(16))
-static const __int64 TENS[2]     = {0x0a0a0a0a0a0a0a0a,0x0a0a0a0a0a0a0a0a}; 
-
-__declspec(align(16))
-static const __int64 FOURS[2] =    {0x0404040404040404,0x0404040404040404}; 
-
-__declspec(align(16))
-static const __int64 ONES[2] =     {0x0101010101010101,0x0101010101010101}; 
-
-__declspec(align(16))
-static const __int64 Max_Comb[2] = {0x0202020202020202,0x0202020202020202}; 
-
-__declspec(align(16))
-static const __int64 WHITE[2] =    {0x7fff0fff7fff0fff,0x7fff0fff7fff0fff};
+static const __int64 YMask = 0x00ff00ff00ff00ff; // keeps only luma
+static const __int64 UVMask =  0xff00ff00ff00ff00; // keeps only chroma
 
 static const __int64 ShiftMask = 0xfefffefffefffeff;	// to avoid shifting chroma to luma
-
-static __int64 swork = 0;
-
-/*
-// A bunch of things that may need 16 byte alignment
-
-__declspec(align(16))
-static const __int64 FIFTEENS[2] = {0x0F0F0F0F0F0F0F0F,0x0F0F0F0F0F0F0F0F}; 
-
-__declspec(align(16))
-static const __int64 TWENTIES[2] = {0x1414141414141414,0x1414141414141414}; 
-
-__declspec(align(16))
-static const __int64 SIXES[2] =    {0x0606060606060606,0x0606060606060606}; 
-
-__declspec(align(16))
-static const __int64 FIVES[2] =    {0x0505050505050505,0x0505050505050505}; 
-
-__declspec(align(16))
-static const __int64 THREES[2] =   {0x0303030303030303,0x0303030303030303}; 
-
-__declspec(align(16))
-static const __int64 TWOS[2] =     {0x0202020202020202,0x0202020202020202}; 
-
-*/
 
 
 // Define a few macros for CPU dependent instructions. 
@@ -200,57 +135,23 @@ static const __int64 TWOS[2] =     {0x0202020202020202,0x0202020202020202};
 		__asm mov   reg, dword ptr [reg].intfield \
 		}
 
-
-#define MERGE4PIXavg(PADDR1, PADDR2) \
-__asm { \
-		__asm movq	mm0, qword ptr[(PADDR1)] /* our 4 pixels */ \
-		__asm movq mm1, qword ptr[(PADDR2)] /* our pixel2 value */ \
-		__asm movq mm2, mm0			/* another copy of our pixel1 value */ \
-		__asm movq mm3, mm1			/* another copy of our pixel1 value */ \
-		__asm psubusb mm2, mm1 \
-		__asm psubusb mm3, mm0 \
-		__asm por mm2,mm3 \
-		__asm V_PAVGB (mm0, mm1, mm3, ShiftMask) /* avg of 2 pixels */ \
-		__asm movq mm3, mm2			/* another copy of our our weights */ \
-		__asm pxor	 mm1, mm1 \
-		__asm psubusb mm3, mm7			/* nonzero where old weights lower, else 0 */ \
-		__asm pcmpeqb mm3, mm1			/* now ff where new better, else 00	*/ \
-		__asm pcmpeqb mm1, mm3		/* here ff where old better, else 00 */ \
-		__asm pand	mm0, mm3			/* keep only better new pixels */ \
-		__asm pand  mm2, mm3			/* and weights */ \
-		__asm pand	mm5, mm1			/* keep only better old pixels */ \
-		__asm pand  mm7, mm1 \
-		__asm por	mm5, mm0			/* and merge new & old vals */ \
-		__asm por	mm7, mm2 \
+// sorts registers a & b
+#define SORT(a,b,temp1) __asm {	\
+   __asm movq temp1, a					\
+   __asm pminub a, b					\
+   __asm pmaxub b, temp1				\
 }
 
-
-#define MERGE4PIXavgH(PADDR1A, PADDR1B, PADDR2A, PADDR2B) \
-__asm { \
-		__asm movq mm0, mmword ptr[(PADDR1A)] /* our 4 pixels */ \
-		__asm movq mm1, mmword ptr[(PADDR2A)] /* our pixel2 value */ \
-		__asm movq mm2, mmword ptr[(PADDR1B)] /* our 4 pixels */ \
-		__asm movq mm3, mmword ptr[(PADDR2B)] /* our pixel2 value */ \
-		__asm V_PAVGB(mm0, mm2, mm2, ShiftMask) \
-		__asm V_PAVGB(mm1, mm3, mm3, ShiftMask) \
-		__asm movq mm2, mm0			/* another copy of our pixel1 value */ \
-		__asm movq mm3, mm1			/* another copy of our pixel1 value */ \
-		__asm psubusb mm2, mm1 \
-		__asm psubusb mm3, mm0 \
-		__asm por mm2,mm3 \
-		__asm V_PAVGB(mm0, mm1, mm3, ShiftMask)	/* avg of 2 pixels */ \
-		__asm movq mm3, mm2			/* another copy of our our weights */ \
-		__asm pxor	 mm1, mm1 \
-		__asm psubusb mm3, mm7			/* nonzero where old weights lower, else 0 */ \
-		__asm pcmpeqb mm3, mm1			/* now ff where new better, else 00	*/ \
-		__asm pcmpeqb mm1, mm3		/* here ff where old better, else 00 */ \
-		__asm pand	mm0, mm3			/* keep only better new pixels */ \
-		__asm pand  mm2, mm3			/* and weights */ \
-		__asm pand	mm5, mm1			/* keep only better old pixels */ \
-		__asm pand  mm7, mm1 \
-		__asm por	mm5, mm0			/* and merge new & old vals */ \
-		__asm por	mm7, mm2 \
+#define PABS(a,b,temp1) __asm {	\
+   __asm movq temp1, b					\
+   __asm psubusb	temp1, a            \
+   __asm psubusb    a, b                \
+   __asm por		a, temp1            \
 }
 
-
-#define RESET_CHROMA	__asm	{por	mm7, UVMask}	
+// a = a where mask, b otherwise; mask = -1
+#define COMBINE(a,b,mask) __asm {	\
+	__asm pand a, mask				\
+	__asm pandn mask, b				\
+	__asm por a, mask				\
+}

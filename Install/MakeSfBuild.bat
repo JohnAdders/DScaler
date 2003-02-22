3f5205351a6e22abf8d30686b5ad7086bddeec20
adcockj <adcockj@users.sourceforge.net>
2003-02-22 12:28:31 +0000

    Added DSRend and now send files to ftp
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3072 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Install/MakeSfBuild.bat
+++ Install/MakeSfBuild.bat
@@ -2,28 +2,47 @@
 echo Dscaler make a sourceforge build batch file
 echo (c) John Adcock 2002
 if "%1" == "" goto usage
+if "%2" == "" goto usage
 cd ..\..
 md DScalerBuild%1
 cd DScalerBuild%1
-cvs -z3 -d:ext:adcockj@cvs.sf.net:/cvsroot/deinterlace co DScaler
+cvs -z3 -d:ext:%2@cvs.sf.net:/cvsroot/deinterlace co DScaler
+cvs -z3 -d:ext:%2@cvs.sf.net:/cvsroot/deinterlace co DSRend
 cd DScaler\Dscaler
 ..\..\..\Dscaler\Release\verinc
 cvs commit -m "Update Verion for release %1"
 cd ..
 pause
+cd ..\DSRend
+7z a -tzip ..\DScaler\DSRend%1src.zip *.* -r
+cd ..\DScaler
 7z a -tzip ..\DScaler%1src.zip *.* -r
-cd DScaler
-call d:\PROGRA~1\MICROS~2\VC98\bin\vcvars32.bat
+cd Help
+"c:\Program Files\HTML Help Workshop\hhc.exe" Dscaler.hhp
+cd ..\DScaler
+call c:\PROGRA~1\MICROS~2\VC98\bin\vcvars32.bat
 msdev Dscaler.dsw /MAKE "All"
-cd ..\Help
-"d:\Program Files\HTML Help Workshop\hhc.exe" Dscaler.hhp
 cd ..\Install
-"e:\Program Files\Inno Setup 2\Compil32.exe" /cc DScaler.iss
+"c:\Program Files\Inno Setup 2\Compil32.exe" /cc DScaler.iss
 copy Output\Setup.exe ..\..\DScaler%1.exe
 cd ..\..
+del /f /q /s DSRend
+rd /s /q DSRend
 del /f /q /s DScaler  
-rd /s /q Dscaler
+rd /s /q DScaler
+echo Break if there was a problem with the above build
+echo Otherwise pressing enter will send the files to the
+echo incoming directory on sourceforge ready to be released
+pause
+echo cd incoming > ftp.txt
+echo bin >> ftp.txt
+echo put DScaler%1.exe >> ftp.txt
+echo put DScaler%1src.zip >> ftp.txt
+echo bye >> ftp.txt
+ftp -s:ftp.txt -A upload.sf.net
+del ftp.txt
 goto endofbatch
 :usage
-echo To use this program enter the build number as a parameter
+echo To use this program enter the build number as parameter 1
+echo and a valid sourceforge user for parameter 2
 :endofbatch

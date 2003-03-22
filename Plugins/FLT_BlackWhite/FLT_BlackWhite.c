e3e247b71c9fbf52014657afdf2b78c9bc0df0f8
Rob Muller <robmuller@users.sourceforge.net>
2003-03-22 19:14:52 +0000

    Implemented MMX code.
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3119 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Plugins/FLT_BlackWhite/FLT_BlackWhite.c
+++ Plugins/FLT_BlackWhite/FLT_BlackWhite.c
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: FLT_BlackWhite.c,v 1.2 2003-03-22 13:16:26 laurentg Exp $
+// $Id: FLT_BlackWhite.c,v 1.3 2003-03-22 19:14:52 robmuller Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 Laurent Garnier.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -19,13 +19,16 @@
 // Change Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.2  2003/03/22 13:16:26  laurentg
+// Use of an help id
+//
 // Revision 1.1  2003/03/13 00:43:59  laurentg
 // New filter black & white
 //
 //
 /////////////////////////////////////////////////////////////////////////////
 
-#include "windows.h"
+#include <windows.h>
 #include "DS_Filter.h"
 #include "..\help\helpids.h"
 
@@ -34,9 +37,10 @@ FILTER_METHOD BlackWhiteMethod;
 long BlackWhite(TDeinterlaceInfo* pInfo)
 {
     char* Pixels;
-    int x;
     int y;
     int Cycles;
+    const __int64 qwLumiMask       = 0x00FF00FF00FF00FF;
+    const __int64 qwChromaHalfMask = 0x8000800080008000;
 
     // Need to have the current field to do the filtering.
     if (pInfo->PictureHistory[0] == NULL)
@@ -44,18 +48,36 @@ long BlackWhite(TDeinterlaceInfo* pInfo)
         return 1000;
     }
 
-    Cycles = pInfo->LineLength / 2;
+    Cycles = pInfo->LineLength / 8;
+    Pixels = pInfo->PictureHistory[0]->pData;
 
     for (y = 0; y < pInfo->FieldHeight; ++y)
     {
-        Pixels = (char*)pInfo->PictureHistory[0]->pData + y * pInfo->InputPitch + 1;
-		for (x = 0; x < Cycles; x++)
-		{
-			*Pixels = (char)128;
-			Pixels+=2;
-		}
+        _asm
+        {
+            mov     eax, Pixels
+            mov     ecx, Cycles
+            movq    mm1, qwLumiMask
+            movq    mm2, qwChromaHalfMask
+
+LOOP_LABEL:            
+            movq    mm0, [eax]
+
+            pand    mm0, mm1
+            por     mm0, mm2
+
+            movq    [eax], mm0
+
+            add     eax, 8
+            dec     ecx
+            jne     LOOP_LABEL          
+        }
+        Pixels += pInfo->InputPitch;
     }
-    
+    _asm 
+    {
+        emms
+    }
 
     return 1000;
 }

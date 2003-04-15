ad4a42b99bf6b8beb5900ebe6aa61ca18f58b94e
adcockj <adcockj@users.sourceforge.net>
2003-04-15 13:07:50 +0000

    Change type from int to list
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3146 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Plugins/FLT_Colorimetry/FLT_Colorimetry.c
+++ Plugins/FLT_Colorimetry/FLT_Colorimetry.c
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: FLT_Colorimetry.c,v 1.1 2003-04-14 12:14:51 adcockj Exp $
+// $Id: FLT_Colorimetry.c,v 1.2 2003-04-15 13:07:50 adcockj Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -18,6 +18,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2003/04/14 12:14:51  adcockj
+// Added new Colorimetry Filter
+//
 /////////////////////////////////////////////////////////////////////////////
 
 #include "windows.h"
@@ -186,9 +189,9 @@ SETTING FLT_ColorimetrySettings[FLT_COLORIMETRY_SETTING_LASTONE] =
         "ColorimetryFilter", "UseColorimetryFilter", NULL,
     },
     {
-        "Colorimetry Type", SLIDER, 0, &ColorimetryType,
+        "Colorimetry Type", ITEMFROMLIST, 0, &ColorimetryType,
         0, 0, COLORIMETRY_LASTONE - 1, 1, 1,
-        NULL,
+        ModeList,
         "ColorimetryFilter", "Colorimetry", Colorimetry_OnChange,
     },
 };

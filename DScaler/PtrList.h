6d14476eb350c4e261dc9082ba1efe674dae6302
atnak <atnak@users.sourceforge.net>
2003-06-06 11:14:40 +0000

    Adding insert and missing next and prev functions.
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3251 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/PtrList.h
+++ DScaler/PtrList.h
@@ -1,5 +1,5 @@
 /////////////////////////////////////////////////////////////////////////////
-// $Id: PtrList.h,v 1.1 2003-06-05 08:38:09 atnak Exp $
+// $Id: PtrList.h,v 1.2 2003-06-06 11:14:40 atnak Exp $
 /////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 Atsushi Nakagawa.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -25,6 +25,9 @@
 // CVS Log
 //
 // $Log: not supported by cvs2svn $
+// Revision 1.1  2003/06/05 08:38:09  atnak
+// Initial release of PtrList template class.
+//
 //
 //////////////////////////////////////////////////////////////////////////////
 
@@ -68,181 +71,218 @@ template <class TPtr>
 class PtrList : protected list<TPtr>
 {
 public:
-	PtrList() : list<TPtr>()
-	{
-		m_iterator = end();
-	};
+    PtrList() : list<TPtr>()
+    {
+        m_iterator = end();
+    };
 
-	virtual ~PtrList()
-	{
-		for (iterator it = begin(); it != end(); it++)
-			delete *it;
-	}
+    virtual ~PtrList()
+    {
+        for (iterator it = begin(); it != end(); it++)
+            delete *it;
+    }
 
-	//  Basic functions:
-	//
-	//  push_back(TPtr ptr);			- Push ptr to back of list*
-	//  push_front(TPtr ptr);			- Push ptr to front of list*
-	//  insert(TPtr ptr);				- Inserts before item at cursor*#
-	//
-	//	void clear();					- Empty the list
-	//	size_type size();				- Get items count of the list
-	//
-	//	* Caller relinquishes ownership of ptr.  Object must be
-	//    delete'able.
-	//
-	//  # See "Iterating functions".
-	//
+    //  Basic functions:
+    //
+    //  push_back(TPtr ptr);            - Push ptr to back of list*
+    //  push_front(TPtr ptr);           - Push ptr to front of list*
+    //  insert(TPtr ptr);               - Inserts before item at cursor*#
+    //
+    //  void clear();                   - Empty the list
+    //  size_type size();               - Get items count of the list
+    //
+    //  * Caller relinquishes ownership of ptr.  Object must be
+    //    delete'able.
+    //
+    //  # See "Iterating functions".
+    //
 
-	void push_back(TPtr ptr) { list<TPtr>::push_back(ptr); };
-	void push_front(TPtr ptr) { list<TPtr>::push_front(ptr); };
-	void insert(TPtr ptr) { list<TPtr>::insert(m_iterator, ptr); };
+    void push_back(TPtr ptr) { list<TPtr>::push_back(ptr); };
+    void push_front(TPtr ptr) { list<TPtr>::push_front(ptr); };
+    void insert(TPtr ptr) { list<TPtr>::insert(m_iterator, ptr); };
 
-	void clear() const
-	{
-		for (iterator it = begin(); it != end(); it++)
-			delete *it;
-		list<TPtr>::clear();
+    void clear()
+    {
+        for (iterator it = begin(); it != end(); it++)
+            delete *it;
+        list<TPtr>::clear();
 
-		m_iterator = end();
-		istack_end_all();
-	}
+        m_iterator = end();
+        istack_end_all();
+    }
 
-	size_type size() const { return list<TPtr>::size(); }
+    size_type size() { return list<TPtr>::size(); }
 
-	//
-	//  Iterating functions:
-	//
-	//  TPtr first();					- Move cursor to first item
-	//  TPtr last();					- Move cursor to last item
-	//
-	//  TPtr current();					- Return item at cursor
-	//  TPtr current(const TPtr ptr);	- Move cursor to ptr
-	//
-	//  TPtr remove();					- Remove item at cursor
-	//  TPtr remove(const TPtr ptr);	- Remove ptr
-	//
-	//  TPtr release();					- Release item at cursor
-	//	TPtr release(const TPtr ptr);	- Release ptr
-	//
+    //
+    //  Iterating functions:
+    //
+    //  TPtr first();                   - Move cursor to first item
+    //  TPtr last();                    - Move cursor to last item
+    //
+    //  TPtr next();                    - Move the cursor to the next item
+    //  TPtr next(const TPtr ptr);      - Move to next item after ptr
+    //  TPtr prev();                    - Move the cursor to the previous item
+    //  TPtr prev(const TPtr ptr);      - Move to previous item before ptr
+    //
+    //  TPtr current();                 - Return item at cursor
+    //  TPtr current(const TPtr ptr);   - Move cursor to ptr
+    //
+    //  TPtr remove();                  - Remove item at cursor
+    //  TPtr remove(const TPtr ptr);    - Remove ptr
+    //
+    //  TPtr release();                 - Release item at cursor
+    //  TPtr release(const TPtr ptr);   - Release ptr
+    //
 
-	//
-	//  Iterator stack functions:
-	//  
-	//	TPtr push_pos();				- Save the current iterator
-	//	TPtr pop_pos();					- Restore the previous iterator
-	//
-
-
-	TPtr first()
-	{
-		m_iterator = begin();
-		return current();
-	}
+    //
+    //  Iterator stack functions:
+    //  
+    //  TPtr push_pos();                - Save the current iterator
+    //  TPtr pop_pos();                 - Restore the previous iterator
+    //
 
 
-	TPtr last()
-	{
-		m_iterator = end();
-		m_iterator--;
-		return current();
-	}
+    TPtr first()
+    {
+        m_iterator = begin();
+        return current();
+    }
 
 
-	TPtr current()
-	{
-		return m_iterator == end() ? NULL : *m_iterator;
-	}
+    TPtr last()
+    {
+        m_iterator = end();
+        m_iterator--;
+        return current();
+    }
 
 
-	TPtr current(const TPtr ptr)
-	{
-		for (m_iterator = begin(); m_iterator != end(); m_iterator++)
-			if (*m_iterator == ptr)
-				return *m_iterator;
-		return NULL;
-	}
+    TPtr next()
+    {
+        if (m_iterator != end())
+        {
+            m_iterator++;
+        }
+        return current();
+    }
 
 
-	TPtr remove()
-	{
-		if (m_iterator != end())
-		{
-			delete *m_iterator;
-			istack_shift_current();
-			m_iterator = erase(m_iterator);
-		}
-		return current();
-	}
+    TPtr next(const TPtr ptr)
+    {
+        return (current(ptr) ? next() : NULL);
+    }
 
 
-	TPtr remove(const TPtr ptr)
-	{
-		return (current(ptr) ? remove() : NULL);
-	}
+    TPtr prev()
+    {
+        if (m_iterator != end())
+        {
+            m_iterator--;
+        }
+        return current();
+    }
 
 
-	TPtr release()
-	{
-		if (m_iterator != end())
-		{
-			TPtr ptr = *m_iterator;
-			istack_shift_current();
-			m_iterator = erase(m_iterator);
-			return ptr;
-		}
-		return NULL;
-	}
+    TPtr prev(const TPtr ptr)
+    {
+        return (current(ptr) ? prev() : NULL);
+    }
 
 
-	TPtr release(const TPtr ptr)
-	{
-		return (current(ptr) ? release() : NULL);
-	}
+    TPtr current()
+    {
+        return m_iterator == end() ? NULL : *m_iterator;
+    }
 
 
-	TPtr push_pos()
-	{
-		m_istack.push_front(m_iterator);
-		return current();
-	}
+    TPtr current(const TPtr ptr)
+    {
+        for (m_iterator = begin(); m_iterator != end(); m_iterator++)
+            if (*m_iterator == ptr)
+                return *m_iterator;
+        return NULL;
+    }
 
 
-	TPtr pop_pos()
-	{
-		if (!m_istack.empty())
-		{
-			m_iterator = m_istack.front();
-			m_istack.pop_front();
-		}
-		return current();
-	}
+    TPtr remove()
+    {
+        if (m_iterator != end())
+        {
+            delete *m_iterator;
+            istack_shift_current();
+            m_iterator = erase(m_iterator);
+        }
+        return current();
+    }
+
+
+    TPtr remove(const TPtr ptr)
+    {
+        return (current(ptr) ? remove() : NULL);
+    }
+
+
+    TPtr release()
+    {
+        if (m_iterator != end())
+        {
+            TPtr ptr = *m_iterator;
+            istack_shift_current();
+            m_iterator = erase(m_iterator);
+            return ptr;
+        }
+        return NULL;
+    }
+
+
+    TPtr release(const TPtr ptr)
+    {
+        return (current(ptr) ? release() : NULL);
+    }
+
+
+    TPtr push_pos()
+    {
+        m_istack.push_front(m_iterator);
+        return current();
+    }
+
+
+    TPtr pop_pos()
+    {
+        if (!m_istack.empty())
+        {
+            m_iterator = m_istack.front();
+            m_istack.pop_front();
+        }
+        return current();
+    }
 
 private:
-	void istack_shift_current()
-	{
-		deque<iterator>::iterator it = m_istack;
-		for ( ; it != m_istack.end(); it++)
-		{
-			if (*it == m_iterator)
-			{
-				(*it)++
-			}
-		}
-	}
+    void istack_shift_current()
+    {
+        deque<iterator>::iterator it = m_istack.begin();
+        for ( ; it != m_istack.end(); it++)
+        {
+            if (*it == m_iterator)
+            {
+                (*it)++;
+            }
+        }
+    }
 
-	void istack_end_all()
-	{
-		deque<iterator>::iterator it = m_istack;
-		for ( ; it != m_istack.end(); it++)
-		{
-			*it = end();
-		}
-	}
+    void istack_end_all()
+    {
+        deque<iterator>::iterator it = m_istack.begin();
+        for ( ; it != m_istack.end(); it++)
+        {
+            *it = end();
+        }
+    }
 
 private:
-	iterator			m_iterator;
-	deque<iterator>		m_istack;
+    iterator            m_iterator;
+    deque<iterator>     m_istack;
 };
 
 

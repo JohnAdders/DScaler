ba8f23825d5a2bf181deeae83e6099fbcd152c1d
adcockj <adcockj@users.sourceforge.net>
2003-10-01 12:23:40 +0000

    Update Verion for release 418
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3416 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- DScaler/VerStub.asm
+++ DScaler/VerStub.asm
@@ -5,7 +5,7 @@
 	public	_gBuildNum
 	public	_gBuildDateTime
 
-_gBuildNum	dd	4364
-_gBuildDateTime	db	"Wed Oct 01 12:50:40 2003",0
+_gBuildNum	dd	4365
+_gBuildDateTime	db	"Wed Oct 01 13:23:38 2003",0
 
 	end

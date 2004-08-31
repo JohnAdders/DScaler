1a4eea1783da09d57b2ac4d4e49fc0fff503d459
adcockj <adcockj@users.sourceforge.net>
2004-08-31 16:02:37 +0000

    Prepare for 4.1.11
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3715 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Api/DScalerVersion.h
+++ Api/DScalerVersion.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// $Id: DScalerVersion.h,v 1.7 2004-08-12 17:03:08 adcockj Exp $
+// $Id: DScalerVersion.h,v 1.8 2004-08-31 16:02:35 adcockj Exp $
 ///////////////////////////////////////////////////////////////////////////////
 // Copyright (c) 2003 John Adcock.  All rights reserved.
 /////////////////////////////////////////////////////////////////////////////
@@ -21,7 +21,7 @@
 // should be updated before each release
 #define DSCALERVERSION_MAJOR 4
 #define DSCALERVERSION_MINOR 1
-#define DSCALERVERSION_REVISION 10
+#define DSCALERVERSION_REVISION 11
 
 // need to have an extra macro for stingify otherwise we get the 
 // name of the definition not the value

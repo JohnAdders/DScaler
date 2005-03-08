e5b83c657cf0e9000cdcfeb8300c682b04f2f773
atnak <atnak@users.sourceforge.net>
2005-03-08 18:14:03 +0000

    Initial upload.
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@3999 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

---
+++ DScaler/TDA8275.cpp
@@ -0,0 +1,258 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: TDA8275.cpp,v 1.1 2005-03-08 18:14:03 atnak Exp $
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2005 Atsushi Nakagawa.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// Sections in this code were ported from video4linux project's "tda8290c"
+// file.  Copyright is unspecified.  List of contributers, as reported by
+// CVS accounts follows: kraxel (revision 1.6)
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+* @file TDA8290.cpp CTDA8290 Implementation
+*/
+
+#include "stdafx.h"
+#include "TDA8275.h"
+#include "TDA8290.h"
+
+
+// TDA8275 frequency dependent parameters (prgTab)
+const CTDA8275::tProgramingParam CTDA8275::k_programmingTable[] =
+{
+	// LOmin,	LOmax,	spd,	BS,		BP,		CP,		GC3,	div1p5
+	{ 55,		62,		3,		2,		0,		0,		3,		1	},
+	{ 62,		66,		3,		3,		0,		0,		3,		1	},
+	{ 66,		76,		3,		1,		0,		0,		3,		0	},
+	{ 76,		84,		3,		2,		0,		0,		3,		0	},
+	{ 84,		93,		3,		2,		0,		0,		1,		0	},
+	{ 93,		98,		3,		3,		0,		0,		1,		0	},
+	{ 98,		109,	3,		3,		1,		0,		1,		0	},
+	{ 109,		123,	2,		2,		1,		0,		1,		1	},
+	{ 123,		133,	2,		3,		1,		0,		1,		1	},
+	{ 133,		151,	2,		1,		1,		0,		1,		0	},
+	{ 151,		154,	2,		2,		1,		0,		1,		0	},
+	{ 154,		181,	2,		2,		1,		0,		0,		0	},
+	{ 181,		185,	2,		2,		2,		0,		1,		0	},
+	{ 185,		217,	2,		3,		2,		0,		1,		0	},
+	{ 217,		244,	1,		2,		2,		0,		1,		1	},
+	{ 244,		265,	1,		3,		2,		0,		1,		1	},
+	{ 265,		302,	1,		1,		2,		0,		1,		0	},
+	{ 302,		324,	1,		2,		2,		0,		1,		0	},
+	{ 324,		370,	1,		2,		3,		0,		1,		0	},
+	{ 370,		454,	1,		3,		3,		0,		1,		0	},
+	{ 454,		493,	0,		2,		3,		0,		1,		1	},
+	{ 493,		530,	0,		3,		3,		0,		1,		1	},
+	{ 530,		554,	0,		1,		3,		0,		1,		0	},
+	{ 554,		604,	0,		1,		4,		0,		0,		0	},
+	{ 604,		696,	0,		2,		4,		0,		0,		0	},
+	{ 696,		740,	0,		2,		4,		1,		0,		0	},
+	{ 740,		820,	0,		3,		4,		0,		0,		0	},
+	{ 820,		865,	0,		3,		4,		1,		0,		0	}
+};
+
+const CTDA8275::tStandardParam CTDA8275::k_standardParamTable[TDA8290_STANDARD_LASTONE] =
+{
+	// sgIFkHz, sgIFLPFilter
+	{ 5750,	1 },
+	{ 6750,	0 },
+	{ 7750,	0 },
+	{ 7750,	0 },
+	{ 7750,	0 },
+	{ 7750,	0 },
+	{ 1250,	0 },
+};
+
+
+CTDA8275::CTDA8275()
+{
+}
+
+CTDA8275::~CTDA8275()
+{
+}
+
+
+void CTDA8275::Attach(CI2CBus* i2cBus, BYTE address)
+{
+	CI2CDevice::Attach(i2cBus, address);
+
+	// Initialize the tuner.
+	WriteTDA8275Initialization();
+}
+
+void CTDA8275::InitializeTuner()
+{
+	WriteTDA8275Initialization();
+}
+
+eVideoFormat CTDA8275::GetDefaultVideoFormat()
+{
+	return VIDEOFORMAT_NTSC_M;
+}
+
+bool CTDA8275::HasRadio() const
+{
+	return false;
+}
+
+bool CTDA8275::SetTVFrequency(long frequencyHz, eVideoFormat videoFormat)
+{
+	if (m_ExternalIFDemodulator != NULL)
+	{
+		m_ExternalIFDemodulator->TunerSet(TRUE, videoFormat);
+	}
+
+	SetFrequency(frequencyHz, CTDA8290::GetTDA8290Standard(videoFormat));
+	m_Frequency = frequencyHz;
+
+	if (m_ExternalIFDemodulator != NULL)
+	{
+		m_ExternalIFDemodulator->TunerSet(FALSE, videoFormat);
+	}
+
+	return true;
+}
+
+bool CTDA8275::SetRadioFrequency(long frequencyHz)
+{
+	return false;
+}
+
+long CTDA8275::GetFrequency()
+{
+	return m_Frequency;
+}
+
+eTunerLocked CTDA8275::IsLocked()
+{
+	BYTE status[3];
+
+	if (ReadFromSubAddress(TDA8275_STATUS, status, 3))
+	{
+		// LOCK = bit 22
+		return (status[2] & 0x40) != 0 ? TUNER_LOCK_ON : TUNER_LOCK_OFF;
+	}
+
+	return TUNER_LOCK_NOTSUPPORTED;
+}
+
+eTunerAFCStatus CTDA8275::GetAFCStatus(long &nFreqDeviation)
+{
+	if (m_ExternalIFDemodulator != NULL)
+	{
+		return m_ExternalIFDemodulator->GetAFCStatus(nFreqDeviation);
+	}
+	return TUNER_AFC_NOTSUPPORTED;
+}
+
+BYTE CTDA8275::GetDefaultAddress() const
+{
+	return I2C_ADDR_TDA8275_1;
+}
+
+void CTDA8275::WriteTDA8275Initialization()
+{
+	// 2 TDA827x Initialization
+	WriteToSubAddress(TDA8275_DB1, 0x00);
+	WriteToSubAddress(TDA8275_DB2, 0x00);
+	WriteToSubAddress(TDA8275_DB3, 0x40);
+	WriteToSubAddress(TDA8275_AB3, 0x2A);
+	WriteToSubAddress(TDA8275_GB, 0xFF);
+	WriteToSubAddress(TDA8275_TB, 0x00);
+	WriteToSubAddress(TDA8275_SDB3, 0x00);
+	WriteToSubAddress(TDA8275_SDB4, 0x40);
+
+	/*
+	// These values come from the "2/ TDA827x Initialization"
+	// code in the data-sheet.  Those that weren't specified
+	// there were substituted with default values from the
+	// default column of the data-sheet.
+
+	BYTE initializationBytes[13] = {
+		// DB1, DB2, DB3, CB1
+		0x00, 0x00, 0x40, 0x70,
+		// BB, AB1, AB2, AB3
+		0x00, 0x83, 0x3F, 0x2A,
+		// AB4, GB, TB, SDB3, SDB4
+		0x04, 0xFF, 0x00, 0x00, 0x40 };
+
+	WriteToSubAddress(TDA8275_DB1, &initializationBytes, 13);
+	*/
+}
+
+bool CTDA8275::SetFrequency(long frequencyHz, eTDA8290Standard standard)
+{
+	int max = sizeof(k_programmingTable)/sizeof(k_programmingTable[0]);
+	int row = 0;
+
+	// Find the matching row of the programming table for this frequency.
+	for ( ; row < max-1; row++)
+		if (k_programmingTable[row].loMax)
+			break;
+
+	const tProgramingParam* table = &k_programmingTable[row];
+	
+	BYTE sgIFLPFilter = k_standardParamTable[(int)standard].sgIFLPFilter;
+	LONG sgIFHz = k_standardParamTable[(int)standard].sgIFkHz * 1000;
+
+	BYTE channelBytes[8];
+
+	// 0.5 is added for rounding.
+	WORD n = (WORD)((1 << table->spd) * ((double)(frequencyHz + sgIFHz) / 400000) + 0.5);
+
+	channelBytes[0] = (n >> 6) & 0xF;
+	channelBytes[1] = (n << 2) & 0xFC;
+	channelBytes[3] = 0x40;
+	channelBytes[4] = sgIFLPFilter ? 0x72 : 0x52; // 7MHz (US) / 9Mhz (Europe)
+	channelBytes[5] = (table->loMax << 6)|(table->div1p5 << 5)|(table->BS << 3)|table->BP;
+	channelBytes[6] = 0x8F | (table->GC3 << 4);
+	channelBytes[7] = 0x8F;
+
+	if (!WriteToSubAddress(TDA8275_DB1, channelBytes, 8) ||
+		!WriteToSubAddress(TDA8275_AB4, 0x00))
+	{
+		return false;
+	}
+
+	bool success = false;
+
+	// 2.2 Re-initialize PLL and gain path
+	success &= WriteToSubAddress(TDA8275_AB2, 0xBF);
+	success &= WriteToSubAddress(TDA8275_CB1, 0xD2);
+	Sleep(1);
+	success &= WriteToSubAddress(TDA8275_CB1, 0x56);
+	Sleep(1); // Only 550us required.
+	success &= WriteToSubAddress(TDA8275_CB1, 0x52);
+	Sleep(550); // 550ms delay required.
+	success &= WriteToSubAddress(TDA8275_CB1, 0x50|table->CP);
+
+	// 3 Enabling VSYNC mode for AGC2
+	WriteToSubAddress(TDA8275_AB2, 0x7F);
+	WriteToSubAddress(TDA8275_AB4, 0x08);
+	return success;
+}
+
+
+
---
+++ DScaler/TDA8275.h
@@ -0,0 +1,133 @@
+/////////////////////////////////////////////////////////////////////////////
+// $Id: TDA8275.h,v 1.1 2005-03-08 18:14:03 atnak Exp $
+/////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (c) 2005 Atsushi Nakagawa.  All rights reserved.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA
+//
+/////////////////////////////////////////////////////////////////////////////
+//
+// $Log: not supported by cvs2svn $
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+* @file TDA8290.h CTDA8290 Header file
+*/
+
+#ifndef __TDA9875_H___
+#define __TDA9875_H___
+
+#include "ITuner.h"
+#include "TDA8290.h"
+
+
+// The I2C addresses for the TDA8275 chip.
+// (These are standard tuner addresses.  a.k.a C0, C2, C4, C6)
+#define I2C_ADDR_TDA8275_0		0x60
+#define I2C_ADDR_TDA8275_1		0x61
+#define I2C_ADDR_TDA8275_2		0x62
+#define I2C_ADDR_TDA8275_3		0x63
+
+
+// Subaddresses used by TDA8275.  Addresses marked R
+// are read mode.  The rest are write mode.
+#define TDA8275_STATUS						0x00	// R
+#define TDA8275_DB1							0x00
+#define TDA8275_DB2							0x10
+#define TDA8275_DB3							0x20
+#define TDA8275_CB1							0x30
+#define TDA8275_BB							0x40
+#define TDA8275_AB1							0x50
+#define TDA8275_AB2							0x60
+#define TDA8275_AB3							0x70
+#define TDA8275_AB4							0x80
+#define TDA8275_GB							0x90
+#define TDA8275_TB							0xA0
+#define TDA8275_SDB3						0xB0
+#define TDA8275_SDB4						0xC0
+
+
+class CTDA8275 : II2CTuner
+{
+public:
+	CTDA8275();
+	virtual ~CTDA8275();
+
+	// Override: Note that the tuner will be initialized upon this call.
+	virtual void Attach(CI2CBus* i2cBus, BYTE address = 0);
+
+	// Initializes the tuner.
+	virtual void InitializeTuner();
+
+	// from ITuner
+
+	// Gets this tuner's tuner ID.
+	virtual eTunerId		GetTunerId() { return TUNER_ABSENT; }//   <------------------------- TODO FIX
+	// Gets the video format that was specified as default on construction.
+	virtual eVideoFormat	GetDefaultVideoFormat();
+	// Gets whether or not this tuner supports radio tuning.
+	virtual bool			HasRadio() const;
+	// The tuner tunes into the specified frequency in TV mode with videoFormat.
+	virtual bool			SetTVFrequency(long frequencyHz, eVideoFormat videoFormat);
+	// The tuner tunes into the specified frequency for radio mode.
+	virtual bool			SetRadioFrequency(long frequencyHz);
+	// Gets the frequency to which the tuner is tuned into.
+	virtual long			GetFrequency();
+
+	// Returns whether or not PLL is locked onto a picture carrier.
+	virtual eTunerLocked	IsLocked();
+	// Gets the AFC status and frequency deviation.
+	virtual eTunerAFCStatus GetAFCStatus(long &nFreqDeviation);
+
+protected:
+	// from CI2CDevice
+
+	// Gets the default I2C bus address for this tuner.
+	virtual BYTE			GetDefaultAddress() const;
+
+protected:
+	// Writes the registers necessary for initialization.
+	virtual void			WriteTDA8275Initialization();
+	// Sets the tuner to tune into a specific frequency.
+	virtual bool			SetFrequency(long frequencyHz, eTDA8290Standard standard);
+
+	typedef struct
+	{
+		WORD	loMin;
+		WORD	loMax;
+		BYTE	spd;
+		BYTE	BS;
+		BYTE	BP;
+		BYTE	CP;
+		BYTE	GC3;
+		BYTE	div1p5;
+	} tProgramingParam;
+
+	typedef struct
+	{
+		WORD	sgIFkHz;
+		BYTE	sgIFLPFilter;
+	} tStandardParam;
+
+	static const tProgramingParam	k_programmingTable[];
+	static const tStandardParam		k_standardParamTable[TDA8290_STANDARD_LASTONE];
+
+private:
+	long			m_Frequency;
+};
+
+
+#endif

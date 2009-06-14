37fc060474674d4d526a56ed069fb9758a245649
adcockj <adcockj@users.sourceforge.net>
2009-06-14 17:04:44 +0000

    Remove crt use from mirror filer
    git-svn-id: https://svn.code.sf.net/p/deinterlace/code/trunk/DScaler@4636 6e12ab15-cf2f-4ffe-bf36-45892a6a5b89

--- Plugins/FLT_Mirror/FLT_Mirror.c
+++ Plugins/FLT_Mirror/FLT_Mirror.c
@@ -86,7 +86,7 @@ __declspec(dllexport) FILTER_METHOD* GetFilterPluginInfo(long CpuFeatureFlags)
     return &MirrorMethod;
 }
 
-BOOL WINAPI DllMain(HANDLE hInst, ULONG ul_reason_for_call, LPVOID lpReserved)
+BOOL WINAPI NoCRTDllMain(HANDLE hInst, ULONG ul_reason_for_call, LPVOID lpReserved)
 {
     return TRUE;
 }
